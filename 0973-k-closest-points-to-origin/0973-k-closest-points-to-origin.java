ba63e2d14b9d51935112dd468e24cfeda4ab7b2d
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-05-25 16:55:45 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -0,0 +1,38 @@
+class Pair{
+    int first;
+    int second;
+    int third;
+    public Pair(int first,int second,int third){
+        this.first=first;
+        this.second=second;
+        this.third=third;
+    }
+}
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        int n=points.length;
+        PriorityQueue<Pair> pq=new  PriorityQueue<>((x,y)->y.first-x.first);
+        for(int i=0;i<n;i++){
+            int x=points[i][0];
+            int y=points[i][1];
+            double dist=Math.sqrt(x*x+y*y);
+            pq.add(new Pair(dist,x,y));
+            if(pq.size()>k){
+                pq.remove();
+            }
+        }
+        int size=pq.size();
+        int ans[][]=new int[size][2];
+        int ind=0;
+        while(pq.size()!=0){
+            Pair pair=pq.remove();
+            int xi=pair.second;
+            int yi=pair.third;
+            
+            ans[ind][0]=xi;
+            ans[ind][1]=yi;
+            ind++;
+        }
+        return ans;
+    }
+}
