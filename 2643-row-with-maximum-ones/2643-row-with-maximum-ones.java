3d39bbd462f2fd41e14cf93dfd924b474c09ca67
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-07-20 21:06:59 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int bsearch(int arr[]){
+        int s=0;
+        int e=arr.length-1;
+        while(s<e){
+            int mid=s+(e-s)/2;
+            if(arr[mid]<1){
+                s=mid+1;
+            }
+            else{
+                e=mid;
+            }
+        }
+        return arr.length-s;
+    }
+    public int[] rowAndMaximumOnes(int[][] mat) {
+        for(int row[]:mat){
+            Arrays.sort(row);
+        }
+        int n=mat.length;
+        int m=mat[0].length;
+        int sum=Integer.MIN_VALUE;
+        int ind=-1;
+        int ans[]=new int[2];
+        for(int arr[]:mat){
+            ind++;
+            int bs=bsearch(arr);
+            if(sum<bs){
+                sum=bs;
+                ans=new int[]{ind,sum};
+            }
+        }
+        return ans;
+    }
+}
