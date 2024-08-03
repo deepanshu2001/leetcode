c06159dcc3d18dfd322be89f52f6d8e279d72961
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-08-02 21:09:49 -0700

    Time: 28 ms (41.31%), Space: 56.1 MB (48.06%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int [] merge(int arr1[],int arr2[]){
+        int arr[]=new int[arr1.length+arr2.length];
+        int i=0;
+        int j=0;
+        int k=0;
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i]>arr2[j]){
+                arr[k++]=arr2[j];
+                j++;
+            }
+            else{
+                arr[k++]=arr1[i];
+                i++;
+            }
+        }
+        while(i<arr1.length){
+            arr[k++]=arr1[i];
+            i++;
+        }
+        while(j<arr2.length){
+            arr[k++]=arr2[j];
+            j++;
+        }
+        return arr;
+    }
+    public int[] sortArray(int[] nums) {
+        if(nums.length==1){
+            return nums;
+        }
+        int mid=nums.length/2;
+        int left[]=sortArray(Arrays.copyOfRange(nums,0,mid));
+        int right[]=sortArray(Arrays.copyOfRange(nums,mid,nums.length));
+        return merge(left,right);
+    }
+}
