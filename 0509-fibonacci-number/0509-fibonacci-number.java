59ab35cc530d5dd8a3bc88f9672b52048539955f
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-08-04 21:59:11 -0700

    Time: 8 ms (51.19%), Space: 39.5 MB (99.7%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int f(int n){
+        if(n<=1){
+            return n;
+        }
+        return f(n-1)+f(n-2);
+    }
+    public int fib(int n) {
+        return f(n);
+    }
+}
