99464bda635f6fdca5eefb71deda7670edb35365
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-08-08 22:59:32 -0700

    Time: 1 ms (41.08%), Space: 42.4 MB (5.95%) - LeetHub

--- 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
+++ 0840-magic-squares-in-grid/0840-magic-squares-in-grid.java
@@ -1,57 +1,47 @@
 class Solution {
-    public boolean helper(int[][] grid, int row, int col) {
-        Set<Integer> uniqueNumbers = new HashSet<>();
-        int[] rowSums = new int[3];
-        int[] colSums = new int[3];
-        int diagonalSum1 = 0;
-        int diagonalSum2 = 0;
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                int num = grid[row + i][col + j];
-
-                // Check if number is between 1 and 9
-                if (num < 1 || num > 9) {
-                    return false;
-                }
-
-                // Check if number is unique
-                if (!uniqueNumbers.add(num)) {
-                    return false;
-                }
-
-                // Update row and column sums
-                rowSums[i] += num;
-                colSums[j] += num;
-
-                // Update diagonals
-                if (i == j) {
-                    diagonalSum1 += num;
-                }
-                if (i + j == 2) {
-                    diagonalSum2 += num;
-                }
-            }
-        }
-
-        // Check if all sums are equal
-        int magicSum = rowSums[0];
-        for (int i = 1; i < 3; i++) {
-            if (rowSums[i] != magicSum || colSums[i] != magicSum) {
+    public boolean helper(int [][]grid,int row,int col){
+       int rows[]=new int[3];
+       int cols[]=new int[3];
+       int diagonal1=0;
+       int diagonal2=0;
+       //row sum;
+       Set<Integer> s=new HashSet<>();
+       for(int i=0;i<3;i++){
+        for(int j=0;j<3;j++){
+            int num=grid[row+i][col+j];
+            if(num<1||num>9){
                 return false;
             }
+            if(!s.isEmpty()&&s.contains(num)){
+                return false;
+            }
+            s.add(num);
+            rows[i]+=num;
+            cols[j]+=num;
+            if(i==j){
+                diagonal1+=num;
+            }
+            if(i+j==2){
+                diagonal2+=num;
+            }
         }
-        return diagonalSum1 == magicSum && diagonalSum2 == magicSum;
+       }
+       int sum=rows[0];
+       for(int i=1;i<3;i++){
+        if(rows[i]!=sum || cols[i]!=sum){
+            return false;
+        }
+       }
+       return sum==diagonal1 && sum==diagonal2;
+       
     }
-
     public int numMagicSquaresInside(int[][] grid) {
-        int ans = 0;
-        int n = grid.length;
-        int m = grid[0].length;
-
-        for (int row = 0; row + 2 < n; row++) {
-            for (int col = 0; col + 2 < m; col++) {
-                if (helper(grid, row, col)) {
+        int ans=0;
+        int n=grid.length;
+        int m=grid[0].length;
+        for(int row=0;row+2<n;row++){
+            for(int col=0;col+2<m;col++){
+                if(helper(grid,row,col)==true){
                     ans++;
                 }
             }
