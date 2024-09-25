05dc969c22810173828d899dfc611d6da91f681a
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-09-25 16:02:15 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,29 +1,18 @@
 class Solution {
-    public int f(int ind,List<Integer> l,int dp[]){
-        if(ind==0){
-            return l.get(0);
-        }
-        if(ind<0){
-            return 0;
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        int pick=l.get(ind)+f(ind-2,l,dp);
-        int notpick=0+f(ind-1,l,dp);
-        return dp[ind]=Math.max(pick,notpick);
-    }
     public int rob(int[] nums) {
         int n=nums.length;
         List<Integer> l1=new ArrayList<>();
         List<Integer> l2=new ArrayList<>();
+        if(nums.length==1){
+            return nums[0];
+        }
         int dp1[]=new int[n-1];
         int dp2[]=new int[n-1];
         Arrays.fill(dp1,-1);
         Arrays.fill(dp2,-1);
-        if(nums.length==1){
-            return nums[0];
-        }
+        dp1[0]=nums[0];
+        dp2[0]=nums[0];
+        
         for(int i=0;i<n-1;i++){
             l1.add(nums[i]);
         }
@@ -31,6 +20,22 @@ class Solution {
             l2.add(nums[i]);
         }
         int size=l1.size();
-        return Math.max(f(size-1,l1,dp1),f(size-1,l2,dp2));
+        for(int i=1;i<size;i++){
+            int pick=l1.get(i);
+            if(i>1){
+                pick+=dp1[i-2];
+            }
+            int notpick=0+dp1[i-1];
+            dp1[i]=Math.max(pick,notpick);
+        }
+         for(int i=1;i<size;i++){
+            int pick=l2.get(i);
+            if(i>1){
+                pick+=dp2[i-2];
+            }
+            int notpick=0+dp2[i-1];
+            dp2[i]=Math.max(pick,notpick);
+        }
+        return Math.max(dp1[size-1],dp2[size-1]);
     }
 }
