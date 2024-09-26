6aab7eecd1cf62f181b0287dcb891ff742105b0a
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-09-26 12:05:57 -0700

    Time: 0 ms (100%), Space: 40.6 MB (18.81%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,28 +1,24 @@
 class Solution {
+    public int f(int row,int col,int dp[][]){
+        
+        if(row==0 && col==0){
+            return 1;
+        }
+        if(row<0||col<0){
+            return 0;
+        }
+        if(dp[row][col]!=-1){
+            return dp[row][col];
+        }
+        int up=f(row-1,col,dp);
+        int left=f(row,col-1,dp);
+        return dp[row][col]=left+up;
+    }
     public int uniquePaths(int m, int n) {
-        //tabulation
         int dp[][]=new int[m][n];
         for(int row[]:dp){
             Arrays.fill(row,-1);
         }
-        dp[0][0]=1;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(i==0 && j==0){
-                    dp[0][0]=1;
-                    continue;
-                }
-                int left=0;
-                int up=0;
-                if(i>0){
-                    up=dp[i-1][j];
-                } 
-                if(j>0){
-                    left=dp[i][j-1];
-                }
-                dp[i][j]=left+up;
-            }
-        }
-        return dp[m-1][n-1];
+        return f(m-1,n-1,dp);
     }
 }
