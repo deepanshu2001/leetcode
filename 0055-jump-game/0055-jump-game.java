7067216b35137313362c45f29e55690a60864399
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-03 14:10:14 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,12 +1,17 @@
 class Solution {
-    public boolean canJump(int[] nums) {
-        int max=0;
-        for(int i=0;i<nums.length;i++){
-            if(i>max){
-                return false;
-            }
-            max=i+nums[i];
+    public boolean f(int ind,int nums[]){
+        if(ind==nums.length-1){
+            return true;
         }
-        return true;
+        int postion=nums[ind]+ind;
+        for(int k=ind+1;k<=postion;k++){
+            if(f(k,nums)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean canJump(int[] nums) {
+        return f(0,nums);
     }
 }
