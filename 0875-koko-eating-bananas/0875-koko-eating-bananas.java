9527ede066889046008f20cea072d843888b4843
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-08 16:54:46 -0700

    Time: 28 ms (17.89%), Space: 45 MB (63.1%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,18 +1,17 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
-       int max=Integer.MIN_VALUE;
-       for(int i=0;i<piles.length;i++){
-        max=Math.max(max,piles[i]);
-       }
         int s=1;
-        int e=max;
+        int e=0;
+        for(int i=0;i<piles.length;i++){
+            e=Math.max(e,piles[i]);
+        }
         while(s<e){
             int mid=s+(e-s)/2;
-            int time=0;
+            int hrs=0;
             for(int i=0;i<piles.length;i++){
-                time+=(int)Math.ceil((piles[i]*1.0)/mid);
+                hrs+=Math.ceil(piles[i]*1.0/mid);
             }
-            if(time>h){
+            if(hrs>h){
                 s=mid+1;
             }
             else{
