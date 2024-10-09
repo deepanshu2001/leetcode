e0fbd509959a1e02643902bab742dcfced4ff256
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-08 17:23:54 -0700

    Time: 14 ms (57.28%), Space: 49.1 MB (12.62%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,39 +1,32 @@
-class Pair{
-    int first;
-    int second;
-    public Pair(int first,int second){
-        this.first=first;
-        this.second=second;
-    }
-}
 class Solution {
+    public class Pair{
+        int first;
+        int second;
+        public Pair(int first,int second){
+            this.first=first;
+            this.second=second;
+        }
+    }
     public int[] topKFrequent(int[] nums, int k) {
-        PriorityQueue<Pair> pq=new PriorityQueue<>((x,y)->x.first-y.first);
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(map.containsKey(nums[i])==false){
-                map.put(nums[i],1);
-            }
-            else{
-                map.put(nums[i],map.get(nums[i])+1);
-            }
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
-        if(map.isEmpty()){
-            int ans[]={};
-            return ans;
-        }
+        PriorityQueue<Pair> pq=new PriorityQueue<>((x,y)->x.first-y.first);
         for(Map.Entry<Integer,Integer> m:map.entrySet()){
             int key=m.getKey();
-            int val=m.getValue();
-            pq.add(new Pair(val,key));
-            if(pq.size()>k){
+            int value=m.getValue();
+            pq.add(new Pair(value,key));
+            while(pq.size()>k){
                 pq.remove();
             }
         }
-        int ans[]=new int[pq.size()];
+        int ans[]=new int[k];
         int ind=0;
-        while(pq.size()!=0){
-           ans[ind++]=pq.remove().second;
+        while(!pq.isEmpty()){
+            Pair p=pq.remove();
+            int n=p.second;
+            ans[ind++]=n;
         }
         return ans;
     }
