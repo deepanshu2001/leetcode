e1a61661689de0f7ce0bf74d7f1705ff8e17b7e1
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-12 14:31:24 -0700

    Time: 5 ms (33.06%), Space: 42.1 MB (54.37%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,33 +1,30 @@
 class Solution {
-    public List<String> helper(String p,String up){
-        if(up.length()==0){
-            List<String> ans=new ArrayList<>();
-            ans.add(p);
-            return ans;
-        }
-        List<String> ans=new ArrayList<>();
-        int digit=up.charAt(0)-'0';
-        int i=(digit-2)*3;
-        if(digit>7){
-            i++;
-        }
-        int len=i+3;
-        if(digit==7 || digit==9){
-            len++;
-        }
-        for(;i<len;i++){
-            char ch=(char)('a'+i);
-            ans.addAll(helper(p+ch,up.substring(1)));
-        }
-        return ans;
+    public void f(String p,String up, List<String> ans){
+      if(up.length()==0){
+        ans.add(p);
+        return ;
+      }
+      int digit=up.charAt(0)-'0';
+      int starting_index=(digit-2)*3;
+      //beacuse 7 has 4 letters,so we need to add that into our cal
+      if(digit>7){
+        starting_index++;
+      }
+      int size=starting_index+3;
+      if(digit==7||digit==9){
+        size++;
+      }
+      for(;starting_index<size;starting_index++){
+        char ch=(char)('a'+starting_index);
+        f(p+ch,up.substring(1),ans);
+      }
     }
     public List<String> letterCombinations(String digits) {
+        List<String> ans=new ArrayList<>();
         if(digits.length()==0){
-            List<String> ans=new ArrayList<>();
             return ans;
         }
-        List<String> ans=new ArrayList<>();
-        ans=helper("",digits);
+        f("",digits,ans);
         return ans;
     }
 }
