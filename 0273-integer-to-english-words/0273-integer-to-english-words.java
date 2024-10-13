a73f71125a8494b185a65a06b6340dda7a68e189
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-13 13:57:52 -0700

    Time: 12 ms (19.45%), Space: 43 MB (13.12%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,31 +1,35 @@
 class Solution {
-    String[] ones = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-
-    String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
-
-    String[] thousands = {"", "Thousand", "Million", "Billion"};
-
-    public String numberToWords(int num) {
-        if (num == 0) return "Zero";
-
-        int i = 0;
-        String words = "";
-
-        while (num > 0) {
-            if (num % 1000 != 0) {
-                words = helper(num % 1000) + thousands[i] + " " + words;
+    String[] BELOW_20 = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+                "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+                "Eighteen", "Nineteen" };
+        String[] TENS = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+        String[] THOUSANDS = { "", "Thousand", "Million", "Billion" };
+    public String helper(int num){
+            if(num==0){
+                return "";
             }
-            num /= 1000;
+            if(num<20){
+                return BELOW_20[num]+" ";
+            }
+            if(num<100){
+                return TENS[num/10]+" "+helper(num%10);
+            }
+            return BELOW_20[num / 100] + " Hundred " + helper(num % 100);
+        }
+    public String numberToWords(int num) {
+        
+        if(num==0){
+            return "Zero";
+        }
+        int i=0;
+        String words="";
+        while(num>0){
+            if(num%1000!=0){
+              words=helper(num%1000)+THOUSANDS[i]+" "+words;
+            }
+            num=num/1000;
             i++;
         }
-
         return words.trim();
-    }
-
-    private String helper(int num) {
-        if (num == 0) return "";
-        else if (num < 20) return ones[num] + " ";
-        else if (num < 100) return tens[num / 10] + " " + helper(num % 10);
-        else return ones[num / 100] + " Hundred " + helper(num % 100);
     }
 }
