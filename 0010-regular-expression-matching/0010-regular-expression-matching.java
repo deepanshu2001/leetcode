acdb4602ad0c767a9f45af114accac8817775dbb
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-18 17:01:02 -0700

    Time: 1 ms (99.97%), Space: 41.9 MB (79.4%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,23 +1,30 @@
 class Solution {
-    public boolean solve(String s,String p){
-        //base case
-        if(p.length()==0){
-            return s.length()==0;
+    public boolean solve(String s, String p, int i, int j, Boolean[][] memo) {
+        // Base case
+        if (j == p.length()) {
+            return i == s.length();
         }
-        boolean flag=false;
-        if(s.length()>0 && (s.charAt(0)==p.charAt(0)||p.charAt(0)=='.')){
-            flag=true;
+
+        // Check memoization
+        if (memo[i][j] != null) {
+            return memo[i][j];
         }
-        if(p.length()>1 && p.charAt(1)=='*'){
-            boolean notake=solve(s,p.substring(2));
-            boolean take=flag && solve(s.substring(1),p);
-            return take||notake;
+
+        boolean flag = (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
+
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+            boolean notake = solve(s, p, i, j + 2, memo); // Skip the '*' pattern
+            boolean take = flag && solve(s, p, i + 1, j, memo); // Use the current character
+            memo[i][j] = take || notake; // Store the result in memo
+        } else {
+            memo[i][j] = flag && solve(s, p, i + 1, j + 1, memo); // Move both pointers
         }
-        else{
-            return flag && solve(s.substring(1),p.substring(1));
-        }
+
+        return memo[i][j];
     }
+
     public boolean isMatch(String s, String p) {
-        return solve(s,p);
+        Boolean[][] memo = new Boolean[s.length() + 1][p.length() + 1];
+        return solve(s, p, 0, 0, memo);
     }
 }
