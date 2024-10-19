7f3909dfa433e5ba9efa3e944ee20fbd037d73bc
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-19 14:18:54 -0700

    Time: 119 ms (6.43%), Space: 45.7 MB (8.69%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int f(int coins[],int amount,int ind){
+    public int f(int coins[],int amount,int ind,int dp[][]){
          //nottake
          if(amount==0){
             return 0;
@@ -7,15 +7,23 @@ class Solution {
          if(ind>=coins.length){
             return (int)Math.pow(10,9);
          }
-         int notake=0+f(coins,amount,ind+1);
+         if(dp[amount][ind]!=-1){
+            return dp[amount][ind];
+         }
+         int notake=0+f(coins,amount,ind+1,dp);
          int take=(int)Math.pow(10,9);
          if(coins[ind]<=amount){
-            take=1+f(coins,amount-coins[ind],ind);
+            take=1+f(coins,amount-coins[ind],ind,dp);
          }
-         return Math.min(take,notake);
+         return dp[amount][ind]=Math.min(take,notake);
     }
     public int coinChange(int[] coins, int amount) {
-        int ans=f(coins,amount,0);
+         int dp[][]=new int[amount+1][coins.length+1];
+         for(int row[]:dp){
+            Arrays.fill(row,-1);
+         }
+        int ans=f(coins,amount,0,dp);
+       
         if(ans==(int)Math.pow(10,9)){
             return -1;
         }
