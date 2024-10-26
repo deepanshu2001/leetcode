c415d44816e93c78b32012f3366da2b357c00f7d
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-26 09:11:42 -0700

    Time: 7 ms (31.96%), Space: 45.1 MB (8.25%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,47 +1,28 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        List<String> list=new ArrayList<>();
+        Stack<Integer> st=new Stack<>();
         for(int i=0;i<tokens.length;i++){
-            list.add(tokens[i]);
-        }
-        int i=0;
-        while(!"+".equals(list.get(i))||!"-".equals(list.get(i))||!"*".equals(list.get(i))||!"/".equals(list.get(i))){
-            i++;
-        }
-        int ans=0;
-        String s1=list.get(i-1);
-        String s2=list.get(i-2);
-        if(list.get(i)=="+"){
-            ans+=Integer.parseInt(s1)+Integer.parseInt(s2);
-        }
-        if(list.get(i)=="-"){
-            ans-=Integer.parseInt(s1)-Integer.parseInt(s2);
-        }
-        if(list.get(i)=="/"){
-            ans/=Integer.parseInt(s1)/Integer.parseInt(s2);
-        }
-        if(list.get(i)=="*"){
-            ans*=Integer.parseInt(s1)*Integer.parseInt(s2);
-        }
-        list.remove(i-1);
-        list.remove(i-2);
-        list.remove(i);
-        i=0;
-        while(list.size()!=0){
-            while(!"+".equals(list.get(i))||!"-".equals(list.get(i))||!"*".equals(list.get(i))||!"/".equals(list.get(i))){
-            i++;
+            String s=tokens[i];
+            if(s.equals("+") || s.equals("-")||s.equals("/")||s.equals("*")){
+                int num2=st.pop();
+                int num1=st.isEmpty()?0:st.pop();
+                if(s.equals("+")){
+                    st.push(num1+num2);
+                }
+                if(s.equals("-")){
+                    st.push(num1-num2);
+                }
+                if(s.equals("/")){
+                    st.push(num1/num2);
+                }
+                if(s.equals("*")){
+                    st.push(num1*num2);
+                }
             }
-            String s=list.get(i-1);
-            if(list.get(i)=="+"){
-                ans+=Integer.parseInt(s);
+            else{
+                st.push(Integer.parseInt(s));
             }
-            if(list.get(i)=="+"){
-                ans+=Integer.parseInt(s);
-            }
-            if(list.get(i)=="+"){
-                ans+=Integer.parseInt(s);
-            }
-            
         }
+        return st.pop();
     }
 }
