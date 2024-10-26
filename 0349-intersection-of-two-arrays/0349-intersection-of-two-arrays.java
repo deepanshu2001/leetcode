3daebda35f8e8ea0de1c73633759a230efbf28ab
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-26 15:22:39 -0700

    Time: 2 ms (96.96%), Space: 43 MB (81.71%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,41 +1,24 @@
 class Solution {
-    public boolean bs(int arr[],int target){
-        int s=0;
-        int e=arr.length-1;
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(arr[mid]>target){
-                e=mid-1;
-            }
-            else if(arr[mid]<target){
-                s=mid+1;
-            }
-            else if(arr[mid]==target){
-               return true;
-            }
-        }
-        return false;
-    }
     public int[] intersection(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        
-        int f=Integer.MAX_VALUE;
-        ArrayList<Integer> a=new ArrayList<>();
+        HashSet<Integer> set1=new HashSet<>();
+        HashSet<Integer> set2=new HashSet<>();
+        List<Integer> ans=new ArrayList<>();
+        for(int i=0;i<nums1.length;i++){
+            set1.add(nums1[i]);
+        }
         for(int i=0;i<nums2.length;i++){
-           if(f==nums2[i]){
-            continue;
-           }
-           if(bs(nums1,nums2[i])==true){
-              a.add(nums2[i]);
-              f=nums2[i];
-           }
-
+            if(set2.contains(nums2[i])){
+                continue;
+            }
+            if(set1.contains(nums2[i])){
+                ans.add(nums2[i]);
+                set2.add(nums2[i]);
+            }
         }
-        int ans[]=new int[a.size()];
-        for(int i=0;i<a.size();i++){
-            ans[i]=a.get(i);
+        int arr[]=new int[ans.size()];
+        for(int i=0;i<ans.size();i++){
+            arr[i]=ans.get(i);
         }
-        return ans;
+        return arr;
     }
 }
