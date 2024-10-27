2bf2770d4d7ae4d25714b518b8f18310cb6bf454
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-27 09:25:05 -0700

    Time: 903 ms (5.17%), Space: 45.2 MB (12.65%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,15 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public String sort(String s){
-        char ch[]=s.toCharArray();
-        Arrays.sort(ch);
-        return new String(ch);
-    }
     public boolean checkInclusion(String s1, String s2) {
-        String s=sort(s1);
-        int len=s.length();
-        for(int i=0;i<=s2.length()-len;i++){
-            String new_string=sort(s2.substring(i,i+len));
-            if(s.equals(new_string)){
+        Map<Character, Integer> map1 = new HashMap<>();
+        for (int i = 0; i < s1.length(); i++) {
+            char ch = s1.charAt(i);
+            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
+        }
+
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            Map<Character, Integer> map2 = new HashMap<>();
+            for (int j = 0; j < s1.length(); j++) {
+                map2.put(s2.charAt(i + j), map2.getOrDefault(s2.charAt(i + j), 0) + 1);
+            }
+            // Compare map1 and map2 outside the inner loop
+            if (map1.equals(map2)) {
                 return true;
             }
         }
