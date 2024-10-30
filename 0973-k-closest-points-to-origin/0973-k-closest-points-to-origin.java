6a7692bc1241ab129e4848cf4b60a51f336d936e
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-30 14:09:53 -0700

    Time: 31 ms (58.18%), Space: 55.3 MB (18.93%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,16 +1,31 @@
 class Solution {
+    class Pair{
+        int first;
+        int second;
+        int third;
+        public Pair(int first,int second,int third){
+            this.first=first;
+            this.second=second;
+            this.third=third;
+        }
+    }
     public int[][] kClosest(int[][] points, int k) {
-        Arrays.sort(points,(a,b)->{
-            return squaredDistance(a)-squaredDistance(b);
-        });
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x, y) -> Integer.compare(y.first, x.first));
+        for(int i=0;i<points.length;i++){
+            int dist=points[i][0]*points[i][0]+points[i][1]*points[i][1];
+            pq.add(new Pair(dist,points[i][0],points[i][1]));
+            while(pq.size()>k){
+                pq.remove();
+            }
+        }
         int ans[][]=new int[k][2];
-        for(int i=0;i<k;i++){
-            ans[i][0]=points[i][0];
-            ans[i][1]=points[i][1];
+        int ind=0;
+        while(!pq.isEmpty()){
+           Pair pair=pq.remove();
+           ans[ind][0]=pair.second;
+           ans[ind][1]=pair.third;
+           ind++;
         }
         return ans;
-    }
-    public int squaredDistance(int arr[]){
-      return arr[0]*arr[0]+arr[1]*arr[1];
     }
 }
