cee0ccf24674ef878817f6624dfe1a29769cd8d0
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-10-30 13:43:48 -0700

    Time: 2 ms (99.48%), Space: 45.4 MB (51.86%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.java
+++ 0539-minimum-time-difference/0539-minimum-time-difference.java
@@ -1,23 +1,31 @@
 class Solution {
     public int findMinDifference(List<String> timePoints) {
-        int ans=Integer.MAX_VALUE;
+        boolean minutes[]=new boolean[24*60];
         for(int i=0;i<timePoints.size();i++){
-            for(int j=i+1;j<timePoints.size();j++){
-                int ind1=timePoints.get(i).indexOf(":");
-                int ind2=timePoints.get(j).indexOf(":");
-                String s1=timePoints.get(i).substring(0,ind1)+timePoints.get(i).substring(ind1+1);
-                String s2=timePoints.get(j).substring(0,ind2)+timePoints.get(j).substring(ind2+1);
-                Integer cal=Math.abs(Integer.parseInt(s1)-Integer.parseInt(s2));
-                int c=cal%100+(cal/100)*60;
-                ans=Math.min(ans,c);
-                if(s2.equals("0000")){
-                    cal=Math.abs(Integer.parseInt(s1)-2360);
-                    c=cal%100+(cal/100)*60;
-                    ans=Math.min(ans,c);
+            int h=Integer.parseInt(timePoints.get(i).substring(0,2));
+            int m=Integer.parseInt(timePoints.get(i).substring(3));
+            int min=h*60+m;
+            if(minutes[min]==true){
+                return 0;
+            }
+            minutes[min]=true;
+        }
+        int ans=Integer.MAX_VALUE;
+        int prev_ind=Integer.MAX_VALUE;
+        int last_Index=Integer.MAX_VALUE;
+        int first_ind=Integer.MAX_VALUE;
+        for(int i=0;i<24*60;i++){
+            if(minutes[i]){
+                if(prev_ind!=Integer.MAX_VALUE){
+                    ans=Math.min(ans,i-prev_ind);
                 }
-                
+                prev_ind=i;
+                if(first_ind==Integer.MAX_VALUE){
+                    first_ind=i;
+                }
+                last_Index=i;
             }
         }
-        return ans;
+        return Math.min(ans,24*60-last_Index+first_ind);
     }
 }
