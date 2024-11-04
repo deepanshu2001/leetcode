3d13789b84021ccf01e1476cf0a5f633306091ce
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-04 12:00:39 -0800

    Time: 1 ms (19.3%), Space: 41.5 MB (31.6%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.java
+++ 0812-rotate-string/0812-rotate-string.java
@@ -1,65 +1,42 @@
 class Solution {
+    private final int PRIME = 101;
 
+  private double calculateHash(String str) {
+    double hash = 0;
+    for(int i=0; i < str.length(); i++) {
+      hash += str.charAt(i) * Math.pow(PRIME, i);
+    }
+    return hash;
+  }
+
+  private double updateHash(double prevHash, char oldChar, char newChar, int patternLength) {
+      double newHash = (prevHash - oldChar) / PRIME;
+    newHash = newHash + newChar * Math.pow(PRIME, patternLength - 1);
+    return newHash;
+  }
+
+  public boolean search(String text, String pattern) {
+    int patternLength = pattern.length();
+      double patternHash = calculateHash(pattern);
+      double textHash = calculateHash(text.substring(0, patternLength));
+
+    for(int i=0; i<= text.length() - patternLength; i++) {
+      if(textHash == patternHash) {
+        if(text.substring(i, i+patternLength).equals(pattern)) {
+          return true;
+        }
+      }
+
+      if (i < text.length() - patternLength) {
+        textHash = updateHash(textHash, text.charAt(i), text.charAt(i + patternLength), patternLength);
+      }
+    }
+    return false;
+  }
     public boolean rotateString(String s, String goal) {
-        // Check if the lengths of both strings are different; if so, they can't be rotations
-        if (s.length() != goal.length()) return false;
-
-        // Concatenate 's' with itself to create a new string containing all possible rotations
-        String doubledString = s + s;
-
-        // Perform KMP substring search to check if 'goal' is a substring of 'doubledString'
-        return kmpSearch(doubledString, goal);
-    }
-
-    private boolean kmpSearch(String text, String pattern) {
-        // Precompute the LPS (Longest Prefix Suffix) array for the pattern
-        int[] lps = computeLPS(pattern);
-        int textIndex = 0, patternIndex = 0;
-        int textLength = text.length(), patternLength = pattern.length();
-
-        // Loop through the text to find the pattern
-        while (textIndex < textLength) {
-            // If characters match, move both indices forward
-            if (text.charAt(textIndex) == pattern.charAt(patternIndex)) {
-                textIndex++;
-                patternIndex++;
-                // If we've matched the entire pattern, return true
-                if (patternIndex == patternLength) return true;
-            }
-            // If there's a mismatch after some matches, use the LPS array to skip unnecessary comparisons
-            else if (patternIndex > 0) {
-                patternIndex = lps[patternIndex - 1];
-            }
-            // If no matches, move to the next character in text
-            else {
-                textIndex++;
-            }
+        if(s.length()!=goal.length()){
+            return false;
         }
-        // Pattern not found in text
-        return false;
-    }
-
-    private int[] computeLPS(String pattern) {
-        int patternLength = pattern.length();
-        int[] lps = new int[patternLength];
-        int length = 0, index = 1;
-
-        // Build the LPS array
-        while (index < patternLength) {
-            // If characters match, increment length and set lps value
-            if (pattern.charAt(index) == pattern.charAt(length)) {
-                length++;
-                lps[index++] = length;
-            }
-            // If there's a mismatch, update length using the previous LPS value
-            else if (length > 0) {
-                length = lps[length - 1];
-            }
-            // No match and length is zero
-            else {
-                lps[index++] = 0;
-            }
-        }
-        return lps;
+        return search(s+s,goal);
     }
 }
