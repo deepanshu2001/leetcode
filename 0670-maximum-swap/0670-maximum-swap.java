97829e04c6cc7ebe6a5157cdbc59032b65b03420
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-08 11:18:38 -0800

    Time: 0 ms (100%), Space: 40.6 MB (27.36%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,25 +1,25 @@
 class Solution {
     public int maximumSwap(int num) {
-        String s = String.valueOf(num);
-        for (int i = 0; i < s.length(); i++) {
-            int max_ind = i;
-            int max = s.charAt(i) - '0'; // Convert character to integer directly
-            for (int j = i + 1; j < s.length(); j++) {
-                if (max < s.charAt(j) - '0') { // Convert character to integer directly
-                    max_ind = j;
-                    max = s.charAt(j) - '0'; // Update max with the new maximum
-                }
+        char ch[]=Integer.toString(num).toCharArray();
+        int maxRightIndex[]=new int[ch.length];
+        int n=ch.length;
+        maxRightIndex[n-1]=n-1;
+        for(int i=n-2;i>=0;i--){
+            if(ch[i]>ch[maxRightIndex[i+1]]){
+                maxRightIndex[i]=i;
             }
-            if (max_ind != i) {
-                char[] ch = s.toCharArray();
-                char temp = ch[i];
-                ch[i] = ch[max_ind];
-                ch[max_ind] = temp;
-                s = new String(ch);
-                break; // Stop after the first swap
+            else{
+                maxRightIndex[i]=maxRightIndex[i+1];
             }
         }
-        
-        return Integer.parseInt(s);
+        for(int i=0;i<n;i++){
+            if(ch[i]<ch[maxRightIndex[i]]){
+                char temp=ch[i];
+                ch[i]=ch[maxRightIndex[i]];
+                ch[maxRightIndex[i]]=temp;
+                return Integer.parseInt(new String(ch));
+            }
+        }
+        return num;
     }
 }
