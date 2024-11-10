88e8c39b362f809c716c2aac851cddaed3c74f2e
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-10 15:43:20 -0800

    Time: 0 ms (100%), Space: 40.6 MB (84%) - LeetHub

--- 0794-valid-tic-tac-toe-state/0794-valid-tic-tac-toe-state.java
+++ 0794-valid-tic-tac-toe-state/0794-valid-tic-tac-toe-state.java
@@ -1,17 +1,61 @@
 class Solution {
-    public boolean validTicTacToe(String[] board) {
-       if(board[0].charAt(0)=='O'){
+    public boolean check(String []board,char player){
+        //checking for row and col
+        for(int i=0;i<board.length;i++){
+            if(board[i].charAt(0)==player && board[i].charAt(1)==player && board[i].charAt(2)==player){
+                return true;
+            }
+            if(board[0].charAt(i)==player && board[1].charAt(i)==player && board[2].charAt(i)==player){
+                return true;
+            }
+        }
+        //checking diagonals
+        if(board[0].charAt(0)==player && board[1].charAt(1)==player && board[2].charAt(2)==player){
+            return true;
+        }
+        if(board[0].charAt(2)==player && board[1].charAt(1)==player && board[2].charAt(0)==player){
+            return true;
+        }
         return false;
-       }
-       for(int i=0;i<board.length;i++){
-        if(!board[i].contains("X")||!board[i].contains("0")){
+    }
+    public boolean validTicTacToe(String[] board) {
+        int x_count=0;
+        int o_count=0;
+        for(int i=0;i<board.length;i++){
+            if(board[i].charAt(0)=='X'){
+                x_count++;
+            }
+            if(board[i].charAt(1)=='X'){
+                x_count++;
+            }
+            if(board[i].charAt(2)=='X'){
+                x_count++;
+            }
+            if(board[i].charAt(0)=='O'){
+                o_count++;
+            }
+            if(board[i].charAt(1)=='O'){
+                o_count++;
+            }
+            if(board[i].charAt(2)=='O'){
+                o_count++;
+            }
+
+        }
+        if(o_count>x_count||x_count>o_count+1){
             return false;
         }
-        if(board[i].equals("XXX")||board[i].equals("OOO")){
+        boolean x_wins=check(board,'X');
+        boolean o_wins=check(board,'O');
+        if(x_wins && o_wins){
             return false;
         }
-        
-       }
-       return true;
+        if(x_wins && x_count!=o_count+1){
+            return false;
+        }
+        if(o_wins && o_count!=x_count){
+            return false;
+        }
+        return true;
     }
 }
