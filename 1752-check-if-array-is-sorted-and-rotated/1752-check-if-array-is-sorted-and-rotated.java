72b0d6d193d961dc90a432c5a891823edfe90963
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-10 13:49:19 -0800

    Time: 0 ms (100%), Space: 41.4 MB (19.39%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,43 +1,22 @@
 class Solution {
     public boolean check(int[] nums) {
-        // Edge case: If the array is empty or has one element, it is always true
-        if (nums.length <= 1) {
-            return true;
-        }
+        int rotationCount = 0;
+        int n = nums.length;
 
-        // Make a copy of the original array and sort it
-        int[] sortedArr = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(sortedArr);
+        // Traverse the array to count the number of rotation points
+        for (int i = 0; i < n; i++) {
+            // Check if the current element is greater than the next element (modulus for circular check)
+            if (nums[i] > nums[(i + 1) % n]) {
+                rotationCount++;
+            }
 
-        // Find the index where the array might have been rotated
-        int rotationPoint = -1;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] > nums[i + 1]) {
-                rotationPoint = i;
-                break;
+            // If there is more than one rotation point, it's not a rotated sorted array
+            if (rotationCount > 1) {
+                return false;
             }
         }
 
-        // If no rotation point is found, the array is already sorted
-        if (rotationPoint == -1) {
-            return true;
-        }
-
-        // Create a new array that simulates the rotation
-        int[] rotatedArr = new int[nums.length];
-        int index = 0;
-
-        // Copy elements from rotation point to the end
-        for (int i = rotationPoint + 1; i < nums.length; i++) {
-            rotatedArr[index++] = nums[i];
-        }
-
-        // Copy elements from the start up to the rotation point
-        for (int i = 0; i <= rotationPoint; i++) {
-            rotatedArr[index++] = nums[i];
-        }
-
-        // Check if the rotated array matches the sorted array
-        return Arrays.equals(rotatedArr, sortedArr);
+        // If zero or one rotation point is found, it's a rotated sorted array
+        return true;
     }
 }
