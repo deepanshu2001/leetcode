48466b09c7c35376265d7cadff287a3afe34ca74
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-10 16:14:50 -0800

    Time: 17 ms (75.98%), Space: 47.3 MB (91.23%) - LeetHub

--- 0706-design-hashmap/0706-design-hashmap.java
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -1,20 +1,65 @@
 class MyHashMap {
-    int data[];
+    public class Entry{
+        int key;
+        int val;
+        public Entry(int key,int val){
+            this.key=key;
+            this.val=val;
+        }
+    }
+    int size=769;
+    LinkedList<Entry> data[];
     public MyHashMap() {
-        data=new int[1000001];
-        Arrays.fill(data,-1);
+        data=new LinkedList[size];
     }
     
     public void put(int key, int value) {
-        data[key]=value;    
+        int bucket=key%size;
+        if(data[bucket]==null){
+            data[bucket]=new LinkedList<Entry>();
+            data[bucket].add(new Entry(key,value));
+        }
+        else{
+            for(Entry entry:data[bucket]){
+                if(entry.key==key){
+                    entry.val=value;
+                    return;
+                }
+            }
+            data[bucket].add(new Entry(key, value));
+        }
     }
     
     public int get(int key) {
-        return data[key];
+        int bucket=key%size;
+        if(data[bucket]==null){
+            return -1;
+        }
+        else{
+            for(Entry entry:data[bucket]){
+                if(entry.key==key){
+                    return entry.val;
+                }
+            }
+        }
+        return -1;
     }
     
     public void remove(int key) {
-        data[key]=-1;
+        int bucket=key%size;
+        if(data[bucket]==null){
+            return;
+        }
+        Entry toRemove=null;
+        for(Entry entry:data[bucket]){
+            if(entry.key==key){
+                toRemove=entry;
+            }
+        }
+        if(toRemove==null){
+            return;
+        }
+        data[bucket].remove(toRemove);
     }
 }
 
