0c5c4f7de6cd5fa12ad4b708e04e66ca36adabe2
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-14 13:59:54 -0800

    Time: 1 ms (80.18%), Space: 41.6 MB (64.41%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -1,21 +1,39 @@
 class Solution {
-    public int f(int ind,String s){
+    public int f(int ind,String s,int dp[]){
         if(ind==s.length()){
-            return 1;
+            return dp[ind]=1;
         }
         if(s.charAt(ind)=='0'){
-            return 0;
+            return dp[ind]=0;
         }
-        int ways=f(ind+1,s);
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+        int ways=f(ind+1,s,dp);
         if(ind+1<s.length() && Integer.parseInt(s.substring(ind,ind+2))<=26){
-            ways+=f(ind+2,s);
+            ways+=f(ind+2,s,dp);
         }
-        return ways;
+        return dp[ind]=ways;
     }
     public int numDecodings(String s) {
         if(s.charAt(0)=='0'){
             return 0;
         }
-        return f(0,s);
+        int dp[]=new int[s.length()+1];
+        dp[s.length()]=1;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)=='0'){
+                dp[i]=0;
+            }
+            else{
+                int ways=dp[i+1];
+                if(i+1<s.length() && Integer.parseInt(s.substring(i,i+2))<=26){
+                    ways+=dp[i+2];
+                }
+                dp[i]=ways;
+            }
+        }
+        
+        return dp[0];
     }
 }
