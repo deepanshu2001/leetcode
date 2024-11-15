41e67914cddd564439f3bfefffdfc5631f59a343
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-14 19:51:15 -0800

    Time: 5 ms (60.8%), Space: 43.3 MB (57.72%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.java
@@ -1,18 +1,33 @@
 class Solution {
     public int minLength(String s) {
-        StringBuilder sb=new StringBuilder(s);
-        for(int i=0;i<sb.length();i++){
-            if(i!=sb.length()-1){
-                if(sb.charAt(i)=='A' && sb.charAt(i+1)=='B'){
-                    sb.delete(i,i+2);
-                    i=-1;
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)!='B' && s.charAt(i)!='D'){
+                st.push(s.charAt(i));
+            }
+            else{
+                if(st.isEmpty()){
+                    st.push(s.charAt(i));
+                    continue;
                 }
-                else if(sb.charAt(i)=='C' && sb.charAt(i+1)=='D'){
-                    sb.delete(i,i+2);
-                    i=-1;
+                if(s.charAt(i)=='B'){
+                    if(st.peek()=='A'){
+                        st.pop();
+                    }
+                    else{
+                        st.push(s.charAt(i));
+                    }
+                }
+                if(s.charAt(i)=='D'){
+                    if(st.peek()=='C'){
+                        st.pop();
+                    }
+                    else{
+                        st.push(s.charAt(i));
+                    }
                 }
             }
         }
-        return sb.length();
+        return st.size();
     }
 }
