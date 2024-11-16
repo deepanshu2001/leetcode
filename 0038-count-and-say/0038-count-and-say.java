3d7d496cca3663bb1b1303a3340f344b572f19ab
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-15 18:01:25 -0800

    Time: 9 ms (31.28%), Space: 41.2 MB (64.57%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,30 +1,31 @@
 class Solution {
-    public String f(int n){
-        if(n==1){
-            return "1";
-        }
-        String s=f(n-1);
-        int i=0;
-       
-        StringBuilder sb=new StringBuilder();
-        int j=i+1;
-        while(i<s.length()){
-        int cnt=1;
-        while(j<s.length() && s.charAt(i)==s.charAt(j)){
-            cnt++;
-            j++;
-        }
-        sb.append(cnt);
-        sb.append(s.charAt(i));
-        i=j;
-        }
-       return sb.toString();
-
-    }
     public String countAndSay(int n) {
-        if(n==1){
+        if (n == 1) {
             return "1";
         }
-        return f(n);
+        StringBuilder s = new StringBuilder();
+        s.append("1");
+        
+        for (int i = 2; i <= n; i++) {
+            int k = 0;
+            StringBuilder sb = new StringBuilder();
+            
+            while (k < s.length()) {
+                int cnt = 1;
+                int j = k + 1;
+                while (j < s.length() && s.charAt(k) == s.charAt(j)) {
+                    cnt++;
+                    j++;
+                }
+                sb.append(cnt);
+                sb.append(s.charAt(k));
+                k = j; // Move to the next group
+            }
+            
+            // Update `s` after processing the entire sequence
+            s = sb;
+        }
+        
+        return s.toString();
     }
 }
