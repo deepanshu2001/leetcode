521d90a45fe07d85ff6ba23abafcbd486d4151a0
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-17 14:53:59 -0800

    Time: 3 ms (74.12%), Space: 48.1 MB (31.28%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,18 +1,20 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int max=0;
+        int min=Integer.MAX_VALUE;
+        int max=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
-            max=Math.max(nums[i],max);
+            min=Math.min(min,nums[i]);
+            max=Math.max(max,nums[i]);
         }
-        int cnt_arr[]=new int[max+1];
+        int cnt_arr[]=new int[max-min+1];
         for(int i=0;i<nums.length;i++){
-            cnt_arr[nums[i]]++;
+            cnt_arr[nums[i]-min]++;
         }
         int cnt=nums.length/3;
         List<Integer> ans=new ArrayList<>();
         for(int i=0;i<cnt_arr.length;i++){
             if(cnt_arr[i]>cnt){
-               ans.add(i);
+               ans.add(i+min);
             }
         }
         return ans;
