6d20ab3fc635049f725e170f5c319556aeb4c7a4
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-17 15:33:14 -0800

    Time: 3 ms (74.12%), Space: 48.1 MB (31.28%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,21 +1,46 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int min=Integer.MAX_VALUE;
-        int max=Integer.MIN_VALUE;
+        List<Integer> ans=new ArrayList<>();
+        int cnt1=0;
+        int cnt2=0;
+        int elem1=Integer.MIN_VALUE;
+        int elem2=Integer.MIN_VALUE;
         for(int i=0;i<nums.length;i++){
-            min=Math.min(min,nums[i]);
-            max=Math.max(max,nums[i]);
-        }
-        int cnt_arr[]=new int[max-min+1];
-        for(int i=0;i<nums.length;i++){
-            cnt_arr[nums[i]-min]++;
+            if(cnt1==0 && nums[i]!=elem2){
+                cnt1++;
+                elem1=nums[i];
+            }
+            else if(cnt2==0 && nums[i]!=elem1){
+                cnt2++;
+                elem2=nums[i];
+            }
+            else if(nums[i]==elem1){
+                cnt1++;
+            }
+            else if(nums[i]==elem2){
+                cnt2++;
+            }
+            else{
+                cnt1--;
+                cnt2--;
+            }
         }
         int cnt=nums.length/3;
-        List<Integer> ans=new ArrayList<>();
-        for(int i=0;i<cnt_arr.length;i++){
-            if(cnt_arr[i]>cnt){
-               ans.add(i+min);
+        cnt1=0;
+        cnt2=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==elem1){
+                cnt1++;
             }
+            if(nums[i]==elem2){
+                cnt2++;
+            }
+        }
+        if(cnt1>cnt){
+            ans.add(elem1);
+        }
+        if(cnt2>cnt){
+            ans.add(elem2);
         }
         return ans;
     }
