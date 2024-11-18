b8a6db0158e2ba8d8e17720b1c1e1a7c7b4e490b
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-18 11:53:00 -0800

    Time: 5 ms (48.68%), Space: 48.5 MB (49.84%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> lexicographicalNumbers = new ArrayList<>();
+        int currentNumber = 1;
+
+       
+        for (int i = 0; i < n; ++i) {
+            lexicographicalNumbers.add(currentNumber);
+
+            if (currentNumber * 10 <= n) {
+                currentNumber *= 10;
+            } else {
+              
+                while (currentNumber % 10 == 9 || currentNumber >= n) {
+                    currentNumber /= 10; // Remove the last digit
+                }
+                currentNumber += 1; 
+            }
+        }
+
+        return lexicographicalNumbers;
+    }
+}
