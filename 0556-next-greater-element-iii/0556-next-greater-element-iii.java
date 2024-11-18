dafcec1c6075e7ef858b9d230cbe031bfaaf194d
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-18 13:52:05 -0800

    Time: 0 ms (100%), Space: 40.3 MB (57.83%) - LeetHub

--- 0556-next-greater-element-iii/0556-next-greater-element-iii.java
+++ 0556-next-greater-element-iii/0556-next-greater-element-iii.java
@@ -1,67 +1,66 @@
-
-
-
 class Solution {
-    public void swap(int i,int j,int nums[]){
-        int temp=nums[i];
-        nums[i]=nums[j];
-        nums[j]=temp;
+    public void swap(int i, int j, int nums[]) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
-    public void reverse(int i,int j,int nums[]){
-        while(i<j){
-            swap(i,j,nums);
+
+    public void reverse(int i, int j, int nums[]) {
+        while (i < j) {
+            swap(i, j, nums);
             i++;
             j--;
         }
     }
+
     public void nextPermutation(int[] nums) {
-        int ind=-1;
-        int n=nums.length-2;
-        //find the break point;
-        for(int i=n;i>=0;i--){
-            if(nums[i]<nums[i+1]){
-                ind=i;
+        int ind = -1;
+        int n = nums.length - 2;
+        // find the break point;
+        for (int i = n; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                ind = i;
                 break;
             }
         }
-        if(ind==-1){
-            reverse(0,nums.length-1, nums);
+        if (ind == -1) {
+            reverse(0, nums.length - 1, nums);
             return;
         }
-        //find the next greater;
-        for(int i=nums.length-1;i>ind;i--){
-            if(nums[i]>nums[ind]){
-                swap(i,ind,nums);
+        // find the next greater;
+        for (int i = nums.length - 1; i > ind; i--) {
+            if (nums[i] > nums[ind]) {
+                swap(i, ind, nums);
                 break;
             }
         }
-        //reverse the res;
-        reverse(ind+1,nums.length-1, nums);
+        // reverse the res;
+        reverse(ind + 1, nums.length - 1, nums);
     }
+
     public int nextGreaterElement(int n) {
-       int cnt=0;
-        int N=n;
-        while(N>0){
-            N=N/10;
+        int cnt = 0;
+        int N = n;
+        while (N > 0) {
             cnt++;
+            N = N / 10;
         }
-        int arr[]=new int[cnt];
-        N=n;
-        int ind=0;
-        while(N>0){
-            int rem=N%10;
-            arr[ind]=rem;
+        int[] arr = new int[cnt];
+        N = n;
+        int ind = 0;
+        while (N > 0) {
+            arr[cnt - ind - 1] = N % 10;
             ind++;
-            N=N/10;
+            N = N / 10;
         }
         nextPermutation(arr);
-        int num=0;
-        for(int i=arr.length-1;i>=0;i--){
-          num=num*10+arr[i];
+        long num = 0;
+        for (int i = 0; i < arr.length; i++) {
+            num = num * 10 + arr[i];
+            if (num > Integer.MAX_VALUE) {
+                return -1;
+            }
         }
-        if(num<=n||num>Integer.MAX_VALUE){
-            return -1;
-        }
-        return num;
+        return (int) (num <= n ? -1 : num);
     }
 }
