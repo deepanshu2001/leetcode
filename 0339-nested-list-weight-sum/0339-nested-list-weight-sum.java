5f43fb11d5bd6957ddedf96867202aedc4847571
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-17 17:13:52 -0800

    Time: 0 ms (100%), Space: 41.4 MB (17.34%) - LeetHub

--- 0339-nested-list-weight-sum/0339-nested-list-weight-sum.java
+++ 0339-nested-list-weight-sum/0339-nested-list-weight-sum.java
@@ -28,16 +28,23 @@
  */
 class Solution {
     public int depthSum(List<NestedInteger> nestedList) {
-        return dfs(nestedList,1);
-    }
-    public int dfs(List<NestedInteger> nestedList,int depth){
-        int total=0;
-        for(NestedInteger nested:nestedList){
-            if(nested.isInteger()){
-                total+=depth*nested.getInteger();
-            }else{
-                total+=dfs(nested.getList(),depth+1);
+        Queue<NestedInteger> queue = new LinkedList<>();
+        queue.addAll(nestedList);
+
+        int depth = 1;
+        int total = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                NestedInteger nested = queue.poll();
+                if (nested.isInteger()) {
+                    total += nested.getInteger() * depth;
+                } else {
+                    queue.addAll(nested.getList());
+                }
             }
+            depth++;
         }
         return total;
     }
