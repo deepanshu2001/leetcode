8148c4d105cb5c65e0bded9e2a7687ddcf93bb2b
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-22 14:03:53 -0800

    Time: 75 ms (8.7%), Space: 45.4 MB (23.87%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.java
@@ -1,29 +1,26 @@
 class Solution {
-    public int f(int start, String s, int[] dp, int n, Set<String> set) {
-        if (start >= n) {
-            return 0;
-        }
-        if (dp[start] != -1) {
-            return dp[start];
-        }
-        int min_extra = Integer.MAX_VALUE;
-        String curr_str = "";
-        for (int i = start; i < n; i++) {
-            curr_str += s.charAt(i);
-            int curr_extra = set.contains(curr_str) ? 0 : curr_str.length();
-            int next_extra = f(i + 1, s, dp, n, set);
-            min_extra = Math.min(min_extra, curr_extra + next_extra);
-        }
-        return dp[start] = min_extra;
-    }
-    
     public int minExtraChar(String s, String[] dictionary) {
         Set<String> set = new HashSet<>();
         for (String word : dictionary) {
             set.add(word);
         }
-        int dp[] = new int[s.length()];
-        Arrays.fill(dp, -1);
-        return f(0, s, dp, s.length(), set);
+        int dp[] = new int[s.length()+1];
+        
+        int n=s.length();
+        dp[n]=0;
+        for(int i=s.length()-1;i>=0;i--){
+            String curr_str = "";
+            int min_extra = Integer.MAX_VALUE;
+            for (int idx =i; idx < n; idx++) {
+                curr_str += s.charAt(idx);
+                int curr_extra = set.contains(curr_str) ? 0 : curr_str.length();
+                int next_extra = dp[idx+1];
+                min_extra = Math.min(min_extra, curr_extra + next_extra);
+                
+            }
+            dp[i]=min_extra;
+
+        }
+        return dp[0];
     }
 }
