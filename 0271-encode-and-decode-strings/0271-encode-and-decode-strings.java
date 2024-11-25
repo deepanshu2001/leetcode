c7e2f3ab9ff3dc3703e6579f55dc318934141125
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-24 17:07:22 -0800

    Time: 12 ms (31.56%), Space: 45.4 MB (57.77%) - LeetHub

--- 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
+++ 0271-encode-and-decode-strings/0271-encode-and-decode-strings.java
@@ -1,30 +1,35 @@
 public class Codec {
-    String s="";
+    StringBuilder sb=new StringBuilder();
     // Encodes a list of strings to a single string.
     public String encode(List<String> strs) {
         for(String str:strs){
-            s+=str;
-            s+="π";
+            sb.append(str.replace("/","//")).append("/:");
         }
-        s=s.substring(0,s.length()-1);
-        return s;
+        return sb.toString();
     }
 
     // Decodes a single string to a list of strings.
     public List<String> decode(String s) {
         List<String> ans=new ArrayList<>();
-        int i=0;
         int j=0;
+        StringBuilder sb=new StringBuilder();
         while(j<s.length()){
-            char ch=s.charAt(j);
-            if(ch=='π'){
-                ans.add(new String(s.substring(i,j)));
-                i=j+1;
-                
-            }
+           char ch=s.charAt(j);
+           if(j+1<s.length() && ch=='/' && s.charAt(j+1)=='/'){
+             sb.append(ch);
+             j=j+2;
+           }
+           else if(j+1<s.length() && ch=='/' && s.charAt(j+1)==':'){
+            ans.add(sb.toString());
+            sb=new StringBuilder();
+            j=j+2;
+           }
+           else{
+            sb.append(ch);
             j++;
+           }
         }
-        ans.add(new String(s.substring(i,j)));
+      
         return ans;
     }
 }
