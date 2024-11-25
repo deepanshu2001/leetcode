ac087e0364d3bcad1519df95769407b53461cd6d
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-24 16:02:04 -0800

    Time: 4 ms (87.66%), Space: 48.6 MB (92.52%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxSubarraySumCircular(int[] nums) {
+        if(nums.length==1){
+            return nums[0];
+        }
+        int ans=Integer.MIN_VALUE;
+        int total_sum=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            total_sum+=nums[i];
+        }
+        //this is when we can find ans in the array without cheking rotated
+        int sum=nums[0];
+        for(int i=1;i<n;i++){
+            sum=Math.max(nums[i],sum+nums[i]);
+            ans=Math.max(ans,sum);
+        }
+        int min_sum=nums[0];
+        int s2=nums[0];
+        for(int i=1;i<n;i++){
+            min_sum=Math.min(nums[i],min_sum+nums[i]);
+            s2=Math.min(s2,min_sum);
+        }
+        int s1=total_sum-s2;
+        if(s1>0){
+            ans=Math.max(ans,s1);
+        }
+        return ans;
+        
+        
+    }
+}
