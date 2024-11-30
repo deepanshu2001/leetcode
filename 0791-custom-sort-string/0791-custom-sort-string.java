8d695513be4347f2d33b0bbe5b41799f58725450
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-11-30 11:54:38 -0800

    Time: 2 ms (69.57%), Space: 42 MB (12.39%) - LeetHub

--- 0791-custom-sort-string/0791-custom-sort-string.java
+++ 0791-custom-sort-string/0791-custom-sort-string.java
@@ -1,18 +1,35 @@
+import java.util.*;
+
 class Solution {
     public String customSortString(String order, String s) {
-       
-        int N = s.length();
-        Character[] result = new Character[N];
-        for (int i = 0; i < N; i++) {
-            result[i] = s.charAt(i);
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
-        Arrays.sort(result, (c1, c2) -> {
-            return order.indexOf(c1) - order.indexOf(c2);
-        });
-        String resultString = "";
-        for (Character c: result) {
-            resultString += c;
+
+        
+        StringBuilder ans = new StringBuilder();
+        for (int i = 0; i < order.length(); i++) {
+            char ch = order.charAt(i);
+            if (map.containsKey(ch)) {
+                int count = map.get(ch);
+                for (int j = 0; j < count; j++) {
+                    ans.append(ch);
+                }
+                map.remove(ch);
+            }
         }
-        return resultString;
+
+
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            char ch = entry.getKey();
+            int count = entry.getValue();
+            for (int j = 0; j < count; j++) {
+                ans.append(ch);
+            }
+        }
+
+        return ans.toString();
     }
 }
