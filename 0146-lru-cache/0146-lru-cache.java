8f8bffd2e04bdc31d3c53b3fc45f7d6c464e1238
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-10 18:00:20 -0800

    Time: 51 ms (29.27%), Space: 114.8 MB (45.89%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,56 +1,63 @@
 class LRUCache {
-    class Node{
-        Node next;
-        Node prev;
-        int key,value;
-        public Node(int key,int value){
+    class DLL{
+        int key;
+        int val;
+        DLL prev;
+        DLL next;
+        DLL(int key,int val){
             this.key=key;
-            this.value=value;
+            this.val=val;
         }
     }
-    int capacity=0;
-    Node head=new Node(0,0);
-    Node tail=new Node(0,0);
-    Map<Integer,Node> map=new HashMap<>(); 
+    Map<Integer,DLL> map;
+    DLL head;
+    DLL tail;
+    int capacity;
+    int size;
     public LRUCache(int capacity) {
+        map=new HashMap<>();
+        size=0;
         this.capacity=capacity;
+        head=new DLL(0,0);
+        tail=new DLL(0,0);
         head.next=tail;
         tail.prev=head;
-
     }
-    public void remove(Node node){
+    public void remove(DLL node){
         map.remove(node.key);
         node.prev.next=node.next;
         node.next.prev=node.prev;
+        size--;
     }
-    public void insert(Node node){
+    public void insert(DLL node){
        map.put(node.key,node);
-       Node temp=head.next;
-       head.next=node;
+       DLL head_next=head.next;
        node.prev=head;
-       node.next=temp;
-       temp.prev=node;
+       head.next=node;
+       node.next=head_next;
+       head_next.prev=node;
+       size++;
     }
     public int get(int key) {
-        if(map.containsKey(key)==false){
+        if(!map.containsKey(key)){
             return -1;
         }
-        Node node=map.get(key);
+        DLL node=map.get(key);
         remove(node);
         insert(node);
-        return node.value;
+        return node.val;
     }
     
     public void put(int key, int value) {
-        Node node=new Node(key,value);
         if(map.containsKey(key)){
             remove(map.get(key));
         }
-        if(map.size()==capacity){
-           remove(tail.prev);
+        if(size==capacity){
+            remove(tail.prev);
         }
-        insert(node);
         
+        insert(new DLL(key,value));
+
     }
 }
 
