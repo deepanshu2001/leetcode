bd24ca37f524567cc4f0b1c6d250ff33d45eeed0
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-10 18:46:14 -0800

    Time: 0 ms (100%), Space: 42 MB (81.72%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -5,6 +5,26 @@ class Solution {
             nums1[i]=nums2[ind];
             ind++;
         }
-        Arrays.sort(nums1);
+        int max=0;
+        int min=0;
+        for(int i=0;i<nums1.length;i++){
+            max=Math.max(nums1[i],max);
+            min=Math.min(nums1[i],min);
+        }
+        int arr[]=new int[max-min+1];
+        for(int i=0;i<nums1.length;i++){
+            arr[nums1[i]-min]++;
+        }
+        ind=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                while(arr[i]>0){
+                   nums1[ind]=i+min;
+                   arr[i]--;
+                   ind++;
+                }
+            }
+        }
+        
     }
 }
