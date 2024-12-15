661f8607c3d4763b4e1489299a19af17db4d6d50
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-14 17:19:08 -0800

    Time: 3 ms (47.12%), Space: 45.8 MB (28.95%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,33 +1,12 @@
 class Solution {
-    boolean flag=false;
-    public boolean f(int start,int nums[],int dp[]){
-        if(start>=nums.length-1){
-           dp[start]=1;
-           return true;
-        }
-        if(dp[start]!=-1){
-            return dp[start]==1?true:false;
-        }
-        for(int i=start+1;i<=start+nums[start];i++){
-            if(f(i,nums,dp)){
-                dp[start]=1;
-                return true;
-            }
-        }
-        dp[start]=0;
-        return false;
-    }
     public boolean canJump(int[] nums) {
-        
-        boolean dp[]=new boolean [nums.length+1];
-        dp[nums.length-1]=true;
-        for(int start=nums.length-2;start>=0;start--){
-          for(int jump=start+1;jump<=start+nums[start];jump++){
-            if(jump<=nums.length-1 && dp[jump]){
-                dp[start]=true;
+        int max=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>max){
+                return false;
             }
-          }
+            max=Math.max(max,i+nums[i]);
         }
-        return dp[0];
+        return true;
     }
 }
