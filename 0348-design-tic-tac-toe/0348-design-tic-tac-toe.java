589f54becfd5a9abb4f9ab7e6fa6dfe05d34b2ce
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-17 13:55:11 -0800

    Time: 3 ms (99.94%), Space: 45.7 MB (26.74%) - LeetHub

--- 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
+++ 0348-design-tic-tac-toe/0348-design-tic-tac-toe.java
@@ -1,36 +1,37 @@
-public class TicTacToe {
-    int[] rows;
-    int[] cols;
-    int diagonal;
-    int antiDiagonal;
-
+class TicTacToe {
+    int rows[];
+    int cols[];
+    int left_dig;
+    int right_diag;
     public TicTacToe(int n) {
-        rows = new int[n];
-        cols = new int[n];
+        rows=new int[n];
+        cols=new int[n];
+        left_dig=0;
+        right_diag=0;
     }
-
+    
     public int move(int row, int col, int player) {
-        int currentPlayer = (player == 1) ? 1 : -1;
-        // update currentPlayer in rows and cols arrays
-        rows[row] += currentPlayer;
-        cols[col] += currentPlayer;
-        // update diagonal
-        if (row == col) {
-            diagonal += currentPlayer;
+        int currentplayer=player==1?1:-1;
+        int n=rows.length;
+        rows[row]+=currentplayer;
+        cols[col]+=currentplayer;
+        //if consists of left digonal
+        if(row==col){
+          left_dig+=currentplayer;
         }
-        //update anti diagonal
-        if (col == (cols.length - row - 1)) {
-            antiDiagonal += currentPlayer;
+        //consist of right digonal
+        if(col==cols.length-row-1){
+            right_diag+=currentplayer;
         }
-        int n = rows.length;
-        // check if the current player wins
-        if (Math.abs(rows[row]) == n ||
-                Math.abs(cols[col]) == n ||
-                Math.abs(diagonal) == n ||
-                Math.abs(antiDiagonal) == n) {
+        if(Math.abs(rows[row])==n||Math.abs(cols[col])==n||Math.abs(left_dig)==n||Math.abs(right_diag)==n){
             return player;
         }
-        // No one wins
         return 0;
     }
 }
+
+/**
+ * Your TicTacToe object will be instantiated and called as such:
+ * TicTacToe obj = new TicTacToe(n);
+ * int param_1 = obj.move(row,col,player);
+ */
