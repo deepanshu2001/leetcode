95ad76b49de6154828b0e3b5b22b8d572cbb36e9
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-17 18:14:54 -0800

    Time: 5 ms (27.33%), Space: 42.7 MB (9.05%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -9,31 +9,25 @@ class Solution {
         '8', "tuv",
         '9', "wxyz"
     );
-    
+
     private List<String> ans = new ArrayList<>();
-    private String phoneNum;
-
-    public void f(StringBuilder s, int ind, String digits) {
-        if (s.length() == phoneNum.length()) {
-            ans.add(s.toString());
-            return;
-        }
-        if (ind >= digits.length()) return;
-
-        char ch = digits.charAt(ind);
-        String mapped_num = letters.get(ch);
-        for (char c : mapped_num.toCharArray()) {
-            s.append(c);       
-            f(s, ind + 1, digits); 
-            s.deleteCharAt(s.length() - 1); 
-        }
-    }
 
     public List<String> letterCombinations(String digits) {
         ans.clear(); 
         if (digits == null || digits.isEmpty()) return ans;
-        phoneNum = digits;
-        f(new StringBuilder(), 0, digits);
+        Queue<String> q = new LinkedList<>();
+        q.add("");
+        for (char ch : digits.toCharArray()) {
+            String possible_comb = letters.get(ch);
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                String comb = q.poll();
+                for (char letter : possible_comb.toCharArray()) {
+                    q.add(comb + letter); 
+                }
+            }
+        }
+        ans.addAll(q);
         return ans;
     }
 }
