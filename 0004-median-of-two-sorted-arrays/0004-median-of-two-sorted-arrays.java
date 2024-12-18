720550d49d15da7c7a8248bb48bde7b55ce81482
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-17 19:17:11 -0800

    Time: 2 ms (59.02%), Space: 46.1 MB (37.27%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,22 +1,60 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int arr[]=new int[nums1.length+nums2.length];
-        int ind=0;
-        for(int i=0;i<nums1.length;i++){
-            arr[ind]=nums1[i];
-            ind++;
+        int ind1=0;
+        int ind2=0;
+        int elem1=0;
+        int elem2=0;
+        int i=0;
+        int j=0;
+        int cnt=0;
+        ind2=(nums1.length+nums2.length)/2;
+        ind1=ind2-1;
+        while(i<nums1.length && j<nums2.length){
+            if(nums1[i]<nums2[j]){
+                if(cnt==ind1){
+                    elem1=nums1[i];
+                }
+                if(cnt==ind2){
+                    elem2=nums1[i];
+                }
+                cnt++;
+                i++;
+            }
+            else{
+                if(cnt==ind1){
+                    elem1=nums2[j];
+                }
+                if(cnt==ind2){
+                    elem2=nums2[j];
+                }
+                cnt++;
+                j++;
+            }
         }
-        for(int i=0;i<nums2.length;i++){
-            arr[ind]=nums2[i];
-            ind++;
+        while(i<nums1.length){
+            if(cnt==ind1){
+                elem1=nums1[i];
+            }
+            if(cnt==ind2){
+                elem2=nums1[i];
+            }
+            cnt++;
+            i++;
         }
-        Arrays.sort(arr);
-        if(arr.length%2==1){
-            return (double)arr[arr.length/2];
+        while(j<nums2.length){
+            if(cnt==ind1){
+                elem1=nums2[j];
+            }
+            if(cnt==ind2){
+                elem2=nums2[j];
+            }
+            cnt++;
+            j++;
         }
-        int l1=arr[arr.length/2-1];
-        int l2=arr[arr.length/2];
-        return (l1+l2)*1.0/2;
+        if((nums1.length+nums2.length)%2==1){
+            return (double)elem2;
+        }
+        return (double)(elem1+elem2)/2;
 
     }
 }
