efb6dc1492790542b8bd7800b8f44ce696cddf81
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-19 18:48:25 -0800

    Time: 1 ms (100%), Space: 41.9 MB (82.46%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,31 +1,30 @@
-
-
-
 class Solution {
-    public boolean solve(String s,String p,Boolean dp[][],int i,int j){
-        //base case
-        if(j==p.length()){
-            return s.length()==i;
+    public boolean solve(String s, String p, int i, int j, Boolean[][] memo) {
+        // Base case
+        if (j == p.length()) {
+            return i == s.length();
         }
-        if(dp[i][j]!=null){
-            return dp[i][j];
+
+        // Check memoization
+        if (memo[i][j] != null) {
+            return memo[i][j];
         }
-        boolean flag=false;
-        if(i<s.length() && (s.charAt(i)==p.charAt(j)||p.charAt(j)=='.')){
-            flag=true;
+
+        boolean flag = (i < s.length() && (s.charAt(i) == p.charAt(j) || p.charAt(j) == '.'));
+
+        if (j + 1 < p.length() && p.charAt(j + 1) == '*') {
+            boolean notake = solve(s, p, i, j + 2, memo); // Skip the '*' pattern
+            boolean take = flag && solve(s, p, i + 1, j, memo); // Use the current character
+            memo[i][j] = take || notake; // Store the result in memo
+        } else {
+            memo[i][j] = flag && solve(s, p, i + 1, j + 1, memo); // Move both pointers
         }
-        if(j+1<p.length() && p.charAt(j+1)=='*'){
-            boolean notake=solve(s,p,dp,i,j+2);
-            boolean take=flag && solve(s,p,dp,i+1,j);
-            return take||notake;
-        }
-        else{
-            return flag && solve(s,p,dp,i+1,j+1);
-        }
+
+        return memo[i][j];
     }
+
     public boolean isMatch(String s, String p) {
-        Boolean dp[][]=new Boolean[s.length()+1][p.length()+1];
-        
-        return solve(s,p,dp,0,0);
+        Boolean[][] memo = new Boolean[s.length() + 1][p.length() + 1];
+        return solve(s, p, 0, 0, memo);
     }
 }
