8a118fd0f20887b1eb9b2e55f8216812a45a41e5
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-20 18:30:58 -0800

    Time: 0 ms (100%), Space: 42.8 MB (77.11%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -4,14 +4,14 @@ class Solution {
         int e=nums.length-1;
         while(s<=e){
             int mid=s+(e-s)/2;
-            if(nums[mid]<target){
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
                 s=mid+1;
             }
-            else if(nums[mid]>target){
-                e=mid-1;
-            }
             else{
-                return mid;
+                e=mid-1;
             }
         }
         return s;
