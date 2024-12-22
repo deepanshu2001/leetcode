cba8dde781561e9b59594d94369b9f4c430b9922
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-22 14:46:00 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0066-plus-one/0066-plus-one.java
+++ 0066-plus-one/0066-plus-one.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int carry=1;
-        for(int i=digits.length-1;i>=0;i--){
-            digits[i]+=carry;
-            if(digits[i]<=9){
-                return digits;
-            }
-            digits[i]=0;
+        int ans=0;
+        int arr[];
+        if(digits[digits.length-1]==9 && digits[0]==9){
+            arr=new int[digits.length+1];
         }
-        int ans[]=new int[digits.length+1];
-        ans[0]=1;
-        return ans;
+        else{
+            arr=new int[digits.length];
+        }
+        for(int i=0;i<digits.length;i++){
+            ans=ans*10+digits[i];
+        }
+        ans+=1;
+        for(int i=arr.length-1;i>=0;i--){
+            arr[i]=ans%10;
+            ans=ans/10;
+        }
+        return arr;
     }
 }
