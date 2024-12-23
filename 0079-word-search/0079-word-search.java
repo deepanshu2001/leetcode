a8349b3c3ac6f5a68558cd9117a77514dc9b0e29
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-23 14:01:02 -0800

    Time: 172 ms (34.22%), Space: 45.7 MB (5.54%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,6 +1,6 @@
 class Solution {
-    public boolean dfs(char [][]board,int [][]vis,int row,int col,String word,int index){
-        if(index==word.length()){
+    public boolean dfs(char [][]board,int [][]vis,int row,int col,String word,int ind){
+        if(ind==word.length()){
             return true;
         }
         vis[row][col]=1;
@@ -9,14 +9,11 @@ class Solution {
         int deltarow[]={-1,0,1,0};
         int deltacol[]={0,1,0,-1};
         for(int i=0;i<4;i++){
-            int nrow=row+deltarow[i];
-            int ncol=col+deltacol[i];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
-                if(board[nrow][ncol]==word.charAt(index)){
-                    if(dfs(board,vis,nrow,ncol,word,index+1)==true){
-                        return true;
-                    }
-                    
+            int nrow=deltarow[i]+row;
+            int ncol=deltacol[i]+col;
+            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0 && word.charAt(ind)==board[nrow][ncol]){
+                if(dfs(board,vis,nrow,ncol,word,ind+1)==true){
+                    return true;
                 }
             }
         }
@@ -26,17 +23,15 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int n=board.length;
         int m=board[0].length;
-        int [][]vis=new int[n][m];
-        int row=-1;
-        int col=-1;
-        char ch=word.charAt(0);
+        int vis[][]=new int[n][m];
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-              if(board[i][j]==ch){
+              if(word.charAt(0)==board[i][j]){
                 
                 if(dfs(board,vis,i,j,word,1)==true){
                     return true;
                 }
+                
               }
             }
         }
