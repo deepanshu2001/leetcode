4e4dad7c984725709b2f19b60ccc61f27d0cd14b
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-24 13:47:04 -0800

    Time: 1 ms (80.97%), Space: 41.7 MB (60.36%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -20,20 +20,7 @@ class Solution {
             return 0;
         }
         int dp[]=new int[s.length()+1];
-        dp[s.length()]=1;
-        for(int i=s.length()-1;i>=0;i--){
-            if(s.charAt(i)=='0'){
-                dp[i]=0;
-            }
-            else{
-                int ways=dp[i+1];
-                if(i+1<s.length() && Integer.parseInt(s.substring(i,i+2))<=26){
-                    ways+=dp[i+2];
-                }
-                dp[i]=ways;
-            }
-        }
-        
-        return dp[0];
+        Arrays.fill(dp,-1);
+        return f(0,s,dp);
     }
 }
