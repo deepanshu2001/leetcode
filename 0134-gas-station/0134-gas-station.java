dadc07e79da4707a8c9b36d897a3c992f96b767f
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-25 12:27:00 -0800

    Time: 2 ms (97.55%), Space: 55.8 MB (86.72%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -3,12 +3,12 @@ class Solution {
         int start=0;
         int neg=0;
         int dist=0;
-        for(int i=0;i<gas.length;i++){
+        for(int i=0;i<cost.length;i++){
             dist+=gas[i]-cost[i];
             if(dist<0){
                 neg+=dist;
-                dist=0;
                 start=i+1;
+                dist=0;
             }
         }
         if(dist+neg<0){
