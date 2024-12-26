9724b28ee66c421daf48d0e9f651e0e46d8d4002
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2024-12-26 15:24:09 -0800

    Time: 707 ms (5.36%), Space: 44.2 MB (93.49%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -1,28 +1,51 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> st=new Stack<>();
-        for(int i=0;i<tokens.length;i++){
-            String s=tokens[i];
-            if(s.equals("+") || s.equals("-")||s.equals("/")||s.equals("*")){
-                int num2=st.pop();
-                int num1=st.isEmpty()?0:st.pop();
-                if(s.equals("+")){
-                    st.push(num1+num2);
-                }
-                if(s.equals("-")){
-                    st.push(num1-num2);
-                }
-                if(s.equals("/")){
-                    st.push(num1/num2);
-                }
-                if(s.equals("*")){
-                    st.push(num1*num2);
+        // Convert array to a mutable List
+        List<String> list = new ArrayList<>(Arrays.asList(tokens));
+
+        // Keep going until there's only one number left in 'list'
+        while (list.size() > 1) {
+            int ind = -1;
+            String op = "";
+
+            // 1) Find the *first* operator
+            for (int i = 0; i < list.size(); i++) {
+                // Use .equals(...) for string comparison
+                if (list.get(i).equals("*") || list.get(i).equals("/") 
+                    || list.get(i).equals("+") || list.get(i).equals("-")) {
+                    ind = i;
+                    op = list.get(i);
+                    break;
                 }
             }
-            else{
-                st.push(Integer.parseInt(s));
+
+            // 2) Grab the two operands immediately before the operator
+            int val1 = Integer.parseInt(list.get(ind - 2));
+            int val2 = Integer.parseInt(list.get(ind - 1));
+
+            // 3) Compute the partial result
+            int result = 0;
+            if (op.equals("*")) {
+                result = val1 * val2;
+            } else if (op.equals("/")) {
+                result = val1 / val2;  // watch out for integer division
+            } else if (op.equals("+")) {
+                result = val1 + val2;
+            } else { // "-"
+                result = val1 - val2;
             }
+
+            // 4) Remove the two operands and the operator
+            //    (remove operator at index 'ind', then val2, then val1)
+            list.remove(ind);       // remove operator
+            list.remove(ind - 1);   // remove second operand
+            list.remove(ind - 2);   // remove first operand
+
+            // 5) Put the computed result back in place of the first operand
+            list.add(ind - 2, String.valueOf(result));
         }
-        return st.pop();
+
+        // Only one element left; that's your RPN result
+        return Integer.parseInt(list.get(0));
     }
 }
