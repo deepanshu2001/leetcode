4c5b98e6c54afcc94acb7fe67537c3ba5713c4b5
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-02 11:37:11 -0800

    Time: 2 ms (85.04%), Space: 45.9 MB (68.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,26 +1,16 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i=0;
-        int j=0;
-        while(i<j && nums[i]!=0){
-            i++;
-        }
-        while(j<nums.length && nums[j]==0){
-            j++;
-        }
-        while(i<nums.length && j<nums.length){
-            if(nums[i]==0 && nums[j]!=0 && i<j){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-               
-            }
+        //Brute force way
+        int arr[]=new int[nums.length];
+        int ind=0;
+        for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                i++;
+               arr[ind]=nums[i];
+               ind++;
             }
-            if(nums[j]==0||j<i){
-                j++;
-            }
+        }
+        for(int i=0;i<nums.length;i++){
+            nums[i]=arr[i];
         }
     }
 }
