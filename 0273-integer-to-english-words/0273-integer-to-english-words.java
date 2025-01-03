37858a7416839bbed71413be394578838802817f
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-03 14:22:54 -0800

    Time: 8 ms (58.4%), Space: 42.6 MB (34.99%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,35 +1,32 @@
 class Solution {
-    String[] BELOW_20 = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
-                "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
-                "Eighteen", "Nineteen" };
-        String[] TENS = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
-        String[] THOUSANDS = { "", "Thousand", "Million", "Billion" };
-    public String helper(int num){
-            if(num==0){
-                return "";
-            }
-            if(num<20){
-                return BELOW_20[num]+" ";
-            }
-            if(num<100){
-                return TENS[num/10]+" "+helper(num%10);
-            }
-            return BELOW_20[num / 100] + " Hundred " + helper(num % 100);
-        }
+    private static final String[] belowTen = { "", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+    private static final String[] belowTwenty = { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+    private static final String[] belowHundred = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
     public String numberToWords(int num) {
-        
-        if(num==0){
+        if (num == 0) {
             return "Zero";
         }
-        int i=0;
-        String words="";
-        while(num>0){
-            if(num%1000!=0){
-              words=helper(num%1000)+THOUSANDS[i]+" "+words;
-            }
-            num=num/1000;
-            i++;
+        return convertToWords(num);
+    }
+    private String convertToWords(int num) {
+        if (num < 10) {
+            return belowTen[num];
         }
-        return words.trim();
+        if (num < 20) {
+            return belowTwenty[num - 10];
+        }
+        if (num < 100) {
+            return belowHundred[num / 10] + (num % 10 != 0 ? " " + convertToWords(num % 10) : "");
+        }
+        if (num < 1000) {
+            return convertToWords(num / 100) + " Hundred" + (num % 100 != 0 ? " " + convertToWords(num % 100) : "");
+        }
+        if (num < 1000000) {
+            return convertToWords(num / 1000) + " Thousand" + (num % 1000 != 0 ? " " + convertToWords(num % 1000) : "");
+        }
+        if (num < 1000000000) {
+            return convertToWords(num / 1000000) + " Million" + (num % 1000000 != 0 ? " " + convertToWords(num % 1000000) : "");
+        }
+        return convertToWords(num / 1000000000) + " Billion" + (num % 1000000000 != 0 ? " " + convertToWords(num % 1000000000) : "");
     }
 }
