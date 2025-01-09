d1da9fc62c38d270c1b4ec9f8777eacf7c016c13
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-09 10:59:41 -0800

    Time: 14 ms (64%), Space: 47 MB (74.81%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,5 +1,5 @@
 class Solution {
-    public class Pair{
+    class Pair{
         int first;
         int second;
         int third;
@@ -9,49 +9,34 @@ class Solution {
             this.third=third;
         }
     }
-    public void bfs(int [][]mat,int i,int j,int [][]ans){
-       Queue<Pair> queue=new LinkedList<>();
-       
-       int deltarow[]={-1,0,1,0};
-       int deltacol[]={0,1,0,-1};
-       int n=mat.length;
-       int m=mat[0].length;
-       int vis[][]=new int[n][m];
-       vis[i][j]=1;
-       queue.add(new Pair(0,i, j));
-       while(!queue.isEmpty()){
-        Pair pair=queue.poll();
-        int dist=pair.first;
-        int row=pair.second;
-        int col=pair.third;
-        if(mat[row][col]==0){
-            ans[i][j]=dist;
-            break;
-        }
-       
-        for(int k=0;k<4;k++){
-            int nrow=row+deltarow[k];
-            int ncol=col+deltacol[k];
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
-                queue.add(new Pair(dist+1,nrow,ncol));
-                vis[nrow][ncol]=1;
-            }
-        }
-        
-       }
-    }
     public int[][] updateMatrix(int[][] mat) {
+        Queue<Pair> q=new LinkedList<>();
         int n=mat.length;
         int m=mat[0].length;
+        int vis[][]=new int[n][m];
         int ans[][]=new int[n][m];
-       
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    ans[i][j]=0;
+                    vis[i][j]=1;
+                    q.add(new Pair(0,i,j));
                 }
-                else{
-                    bfs(mat,i,j,ans);
+            }
+        }
+        int deltarow[]={-1,0,1,0};
+        int deltacol[]={0,1,0,-1};
+        while(!q.isEmpty()){
+            Pair p=q.remove();
+            int dis=p.first;
+            int row=p.second;
+            int col=p.third;
+            ans[row][col]=dis;
+            for(int i=0;i<4;i++){
+                int nrow=deltarow[i]+row;
+                int ncol=deltacol[i]+col;
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]==0){
+                    vis[nrow][ncol]=1;
+                    q.add(new Pair(dis+1,nrow,ncol));
                 }
             }
         }
