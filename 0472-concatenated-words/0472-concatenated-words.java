5774a7bfbb083b10c4fcde06c4761ab622d9e650
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-10 11:12:59 -0800

    Time: 79 ms (25.11%), Space: 53 MB (30.99%) - LeetHub

--- 0472-concatenated-words/0472-concatenated-words.java
+++ 0472-concatenated-words/0472-concatenated-words.java
@@ -1,9 +1,9 @@
 class Solution {
-    public boolean check(String word,Set<String> set, Map<String,Boolean> map){
+    public boolean check(String word,Set<String> set,Map<String,Boolean> map){
         if(map.get(word)!=null){
             return map.get(word);
         }
-        for(int i=1;i<word.length();i++){
+        for(int i=1;i<=word.length();i++){
             String prefix=word.substring(0,i);
             String suffix=word.substring(i);
             if(set.contains(prefix) && (set.contains(suffix)||check(suffix,set,map))){
@@ -15,9 +15,12 @@ class Solution {
         return false;
     }
     public List<String> findAllConcatenatedWordsInADict(String[] words) {
-        List<String> ans=new ArrayList<>();
+         List<String> ans=new ArrayList<>();
         Map<String,Boolean> map=new HashMap<>();
-        Set<String> set=new HashSet<>(Arrays.asList(words));
+        Set<String> set=new HashSet<>();
+        for(String word:words){
+            set.add(word);
+        }
         for(String word:words){
             if(check(word,set,map)){
                 ans.add(word);
