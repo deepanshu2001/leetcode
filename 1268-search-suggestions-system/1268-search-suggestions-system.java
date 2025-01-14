e3ccb5ec62e7f6171ca3c747c8ff4613ae9616be
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-14 11:59:53 -0800

    Time: 17 ms (74.6%), Space: 47.4 MB (65.28%) - LeetHub

--- 1268-search-suggestions-system/1268-search-suggestions-system.java
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -1,43 +1,43 @@
 class Solution {
-    public void bs(String searchWord, String[] products, List<String> list) {
-        int s = 0;
-        int e = products.length - 1;
-        int startIndex = -1;
-
-        // Binary search to find the first occurrence of a prefix match
-        while (s <= e) {
-            int mid = s + (e - s) / 2;
-            if (products[mid].startsWith(searchWord)) {
-                startIndex = mid;
-                e = mid - 1; // Continue searching on the left to find the first match
-            } else if (products[mid].compareTo(searchWord) < 0) {
-                s = mid + 1;
-            } else {
-                e = mid - 1;
+    public void bs(String[] products,List<String> temp,String target){
+        int s=0;
+        int e=products.length-1;
+        int start_index=-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(products[mid].startsWith(target)){
+                start_index=mid;
+                e=mid-1;
+            }
+            else if(products[mid].compareTo(target)<0){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
             }
         }
-
-        // Collect up to 3 suggestions from the found starting point
-        if (startIndex != -1) {
-            int count = 0;
-            for (int i = startIndex; i < products.length && count < 3; i++) {
-                if (products[i].startsWith(searchWord)) {
-                    list.add(products[i]);
-                    count++;
-                } else {
+        if(start_index!=-1){
+            int cnt=0;
+            for(int i=start_index;i<products.length && cnt<3;i++){
+                if(products[i].startsWith(target)){
+                    cnt++;
+                    temp.add(products[i]);
+                }
+                else{
                     break;
                 }
             }
         }
     }
-
     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-        List<List<String>> ans = new ArrayList<>();
         Arrays.sort(products);
-        for (int i = 1; i <= searchWord.length(); i++) {
-            List<String> list = new ArrayList<>();
-            bs(searchWord.substring(0, i), products, list);
-            ans.add(new ArrayList<>(list));
+        List<List<String>> ans=new ArrayList<>();
+        String s="";
+        for(int i=0;i<searchWord.length();i++){
+            s+=searchWord.charAt(i);
+            List<String> temp=new ArrayList<>();
+            bs(products,temp,s);
+            ans.add(new ArrayList<>(temp));
         }
         return ans;
     }
