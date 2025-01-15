dc949f4bb41af5781a463073e69cf2e70df7e120
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-15 12:26:50 -0800

    Time: 0 ms (100%), Space: 41.9 MB (20.61%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,42 +1,9 @@
 class Solution {
-    private final int PRIME = 101;
-
-  private double calculateHash(String str) {
-    double hash = 0;
-    for(int i=0; i < str.length(); i++) {
-      hash += str.charAt(i) * Math.pow(PRIME, i);
-    }
-    return hash;
-  }
-
-  private double updateHash(double prevHash, char oldChar, char newChar, int patternLength) {
-      double newHash = (prevHash - oldChar) / PRIME;
-    newHash = newHash + newChar * Math.pow(PRIME, patternLength - 1);
-    return newHash;
-  }
-
-  public boolean search(String text, String pattern) {
-    int patternLength = pattern.length();
-      double patternHash = calculateHash(pattern);
-      double textHash = calculateHash(text.substring(0, patternLength));
-
-    for(int i=0; i<= text.length() - patternLength; i++) {
-      if(textHash == patternHash) {
-        if(text.substring(i, i+patternLength).equals(pattern)) {
-          return true;
-        }
-      }
-
-      if (i < text.length() - patternLength) {
-        textHash = updateHash(textHash, text.charAt(i), text.charAt(i + patternLength), patternLength);
-      }
-    }
-    return false;
-  }
     public boolean rotateString(String s, String goal) {
         if(s.length()!=goal.length()){
             return false;
         }
-        return search(s+s,goal);
+        s=s+s;
+        return s.contains(goal);
     }
 }
