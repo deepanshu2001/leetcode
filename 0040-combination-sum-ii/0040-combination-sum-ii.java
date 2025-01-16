4e7e7824f3e8b3130013fab9a7a2175a435fda8a
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-16 11:54:30 -0800

    Time: 3 ms (77.78%), Space: 43 MB (89.99%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,26 +1,26 @@
 class Solution {
     List<List<Integer>> ans=new ArrayList<>();
-    public void helper(int ind,int target,List<Integer> curr,int[] candidates){
-        if(target==0){
-            ans.add(new ArrayList<>(curr));
-        }
-        for(int i=ind;i<candidates.length;i++){
-            if(i>ind && candidates[i]==candidates[i-1]){
-                continue;
-            }
-            if(candidates[i]>target){
-                break;
-            }
-            curr.add(candidates[i]);
-            helper(i+1,target-candidates[i],curr,candidates);
-            curr.remove(curr.size()-1);
-            
-        }
+    public void f(int ind,int []candidates,int target,List<Integer> temp){
+       if(target==0){
+        ans.add(new ArrayList<>(temp));
+        return;
+       }
+       for(int i=ind;i<candidates.length;i++){
+         if(i>ind && candidates[i]==candidates[i-1]){
+            continue;
+         }
+         if(candidates[i]>target){
+            break;
+         }
+         temp.add(candidates[i]);
+         f(i+1,candidates,target-candidates[i],temp);
+         temp.remove(temp.size()-1);
+       }
     }
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        List<Integer> curr=new ArrayList<>();
-        helper(0,target,curr,candidates);
+        List<Integer> temp=new ArrayList<>();
+        f(0,candidates,target,temp);
         return ans;
     }
 }
