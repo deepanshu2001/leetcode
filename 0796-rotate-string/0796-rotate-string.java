d8841640faf233c57c42eccae71831f906d94947
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-16 11:12:53 -0800

    Time: 1 ms (23.13%), Space: 41.5 MB (61.98%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -1,9 +1,55 @@
 class Solution {
+    public int[] findLPS(String pattern){
+        int m=pattern.length();
+        int LPS[]=new int[m];
+        LPS[0]=0;
+        int j=1;
+        int i=0;
+        while(j<m){
+            if(pattern.charAt(i)==pattern.charAt(j)){
+                i++;
+                LPS[j]=i;
+                j++;
+            }
+            else{
+                if(i!=0){
+                    i=LPS[i-1];
+                }
+                else{
+                    LPS[j]=0;
+                    j++;
+                }
+            }
+        }
+        return LPS;
+    }
     public boolean rotateString(String s, String goal) {
         if(s.length()!=goal.length()){
             return false;
         }
+        int []LPS=findLPS(goal);
         s=s+s;
-        return s.contains(goal);
+        int m=goal.length();
+        int n=s.length();
+        int i=0;
+        int j=0;
+        while(i<n){
+            if(s.charAt(i)==goal.charAt(j)){
+                i++;
+                j++;
+            }
+            if(j==m){
+                return true;
+            }
+            else if(i<n && s.charAt(i)!=goal.charAt(j)){
+                if(j!=0){
+                    j=LPS[j-1];
+                }
+                else{
+                    i++;
+                }
+            }
+        }
+        return false;
     }
 }
