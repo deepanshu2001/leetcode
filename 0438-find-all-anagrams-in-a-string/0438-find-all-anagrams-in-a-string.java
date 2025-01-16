b4d5acd6aec24c9d2d4fb863091e66952db7f333
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-16 13:55:22 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,47 +1,36 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> ans = new ArrayList<>();
-        Map<Character, Integer> map1 = new HashMap<>();
-        
-        // Initialize map1 with counts of characters in p
-        for (int i = 0; i < p.length(); i++) {
-            char ch = p.charAt(i);
-            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
-        }
-        
-        int k = p.length();
-        Map<Character, Integer> map2 = new HashMap<>();
-        int i = 0;
-        int j = 0;
-        
-        while (j < s.length()) {
-            char ch = s.charAt(j);
-            
-            // Increment the count for the current character in map2
-            map2.put(ch, map2.getOrDefault(ch, 0) + 1);
-            
-            if (j - i + 1 < k) {
-                j++;
-            } else if (j - i + 1 == k) {
-                if (map1.equals(map2)) {
-                    ans.add(i);
-                }
-                
-                // Remove or decrement the count of the character at index i in map2
-                char c = s.charAt(i);
-                if (map2.get(c) == 1) {
-                    map2.remove(c);
-                } else {
-                    map2.put(c, map2.get(c) - 1);
-                }
-                
-                i++;
-                j++;
-            }
-        }
-        
+       List<Integer> ans=new ArrayList<>();
+       if(s.length()<p.length()){
         return ans;
+       }
+       Map<Character,Integer> map1=new HashMap<>();
+       for(int i=0;i<p.length();i++){
+        char ch=s.charAt(i);
+        map1.put(ch,map1.getOrDefault(ch,0)+1);
+       }
+       int i=0;
+       int j=0;
+       int k=p.length();
+       Map<Character,Integer> map2=new HashMap<>();
+       while(j<s.length()){
+         char ch=s.charAt(j);
+         map2.put(ch,map2.getOrDefault(ch,0)+1);
+         if(j-i+1>k){
+            char c=s.charAt(i);
+            map2.put(c,map2.get(c)-1);
+            if(map2.get(c)==0){
+                map2.remove(c);
+            }
+            i++;
+         }
+         if(map1.equals(map2)){
+            ans.add(i);
+         }
+         j++;
+         }
+         
+       
+       return ans;
     }
 }
