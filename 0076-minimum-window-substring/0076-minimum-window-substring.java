9aea4a32a3728cef0aff9278bc8727f330286a7c
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-21 12:49:34 -0800

    Time: 131 ms (10.61%), Space: 45.5 MB (35.91%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -2,13 +2,13 @@ class Solution {
     public String minWindow(String s, String t) {
         String ans="";
         int min=Integer.MAX_VALUE;
-        int j=0;
-        int i=0;
         Map<Character,Integer> map=new HashMap<>();
-        for(int k=0;k<t.length();k++){
-            char c=t.charAt(k);
-            map.put(c,map.getOrDefault(c,0)+1);
+        for(int i=0;i<t.length();i++){
+            char ch=t.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
         }
+        int i=0;
+        int j=0;
         int cnt=map.size();
         while(j<s.length()){
             char ch=s.charAt(j);
@@ -17,10 +17,10 @@ class Solution {
                 continue;
             }
             else{
-                map.put(ch,map.get(ch)-1);
-                if(map.get(ch)==0){
-                    cnt--;
-                }
+               map.put(ch,map.get(ch)-1);
+               if(map.get(ch)==0){
+                cnt--;
+               }
             }
             if(cnt==0){
                 if(min>j-i+1){
@@ -28,10 +28,10 @@ class Solution {
                     min=j-i+1;
                 }
                 while(cnt==0){
-                    char c=s.charAt(i);
-                    if(map.containsKey(c)){
-                        map.put(c,map.get(c)+1);
-                        if(map.get(c)>0){
+                    char new_char=s.charAt(i);
+                    if(map.containsKey(new_char)){
+                        map.put(new_char,map.get(new_char)+1);
+                        if(map.get(new_char)>0){
                             cnt++;
                         }
                     }
