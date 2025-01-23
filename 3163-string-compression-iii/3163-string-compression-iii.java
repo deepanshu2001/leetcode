4dfe0a3ffa664084542754d6e138570d7fa921c8
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-23 11:45:23 -0800

    Time: 17 ms (84.02%), Space: 45.9 MB (58.86%) - LeetHub

--- 3163-string-compression-iii/3163-string-compression-iii.java
+++ 3163-string-compression-iii/3163-string-compression-iii.java
@@ -1,25 +1,18 @@
 class Solution {
     public String compressedString(String word) {
-        StringBuilder sb = new StringBuilder();
-int i = 0;
-
-while (i < word.length()) {
-    char ch = word.charAt(i);
-    int cnt = 1;
-    int j = i + 1;
-
-    // Count consecutive occurrences of the same character, up to a maximum of 9
-    while (j < word.length() && ch == word.charAt(j) && cnt < 9) {
-        cnt++;
-        j++;
-    }
-
-    sb.append(cnt);
-    sb.append(ch);
-    i = j;  // Move the pointer `i` to the position after the counted sequence
-}
-
-return sb.toString();
-
+        StringBuilder comp=new StringBuilder();
+        for(int i=0;i<word.length();i++){
+           char ch=word.charAt(i);
+           int j=i;
+           int cnt=0;
+           while(j<word.length() && cnt<9 && ch==word.charAt(j)){
+            cnt++;
+            j++;
+           }
+           i=j-1;
+           comp.append(cnt);
+           comp.append(ch);
+        }
+        return comp.toString();
     }
 }
