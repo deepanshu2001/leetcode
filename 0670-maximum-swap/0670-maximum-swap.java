836d270b3983a37f632dac21e71118b1556965cb
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-25 14:42:32 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0670-maximum-swap/0670-maximum-swap.java
+++ 0670-maximum-swap/0670-maximum-swap.java
@@ -1,18 +1,31 @@
 class Solution {
     public int maximumSwap(int num) {
-        String numstr=Integer.toString(num);
-        int n=numstr.length();
-        int ans=num;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-               char ch[]=numstr.toCharArray();
-               char temp=ch[i];
-               ch[i]=ch[j];
-               ch[j]=temp;
-               int new_num=Integer.parseInt(new String(ch));
-               ans=Math.max(ans,new_num);
+        char[] ch = Integer.toString(num).toCharArray();
+        int n = ch.length;
+        
+        // Array to store the index of the maximum digit to the right of each position
+        int[] rightMaxIndex = new int[n];
+        rightMaxIndex[n - 1] = n - 1;
+        
+        // Populate the rightMaxIndex array
+        for (int i = n - 2; i >= 0; i--) {
+            rightMaxIndex[i] = (ch[i] < ch[rightMaxIndex[i + 1]]) ? rightMaxIndex[i + 1] : i;
+        }
+
+        // Traverse the array to find the first position where a swap increases the number
+        for (int i = 0; i < n; i++) {
+            if (ch[i] < ch[rightMaxIndex[i]]) {
+                // Perform the swap
+                char temp = ch[i];
+                ch[i] = ch[rightMaxIndex[i]];
+                ch[rightMaxIndex[i]] = temp;
+                
+                // Convert back to integer and return
+                return Integer.parseInt(new String(ch));
             }
         }
-        return ans;
+        
+        // If no swap can improve the number, return the original number
+        return num;
     }
 }
