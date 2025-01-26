39bd6359803dc473281d9147490755b47cf2e400
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-26 13:42:38 -0800

    Time: 5 ms (59.61%), Space: 45.5 MB (85.58%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.java
@@ -1,34 +1,36 @@
 class Solution {
-    public void dfs(int[][] heights,int row,int col,int [][]res,int height,int []deltarow,int []deltacol){
-        int n=heights.length;
-        int m=heights[0].length;
-        if(row<0||row>=n||col<0||col>=m||res[row][col]==1||heights[row][col]<height){
-            return;
-        }
-        res[row][col]=1;
-        for(int i=0;i<4;i++){
-            int nrow=row+deltarow[i];
-            int ncol=col+deltacol[i];
-            dfs(heights,nrow,ncol,res,heights[row][col],deltarow,deltacol);
-        }
+    List<List<Integer>> ans=new ArrayList<>();
+    int deltarow[]={-1,0,1,0};
+    int deltacol[]={0,1,0,-1};
+    public void dfs(int row,int col,int height,int arr[][],int [][]heights){
+       int n=heights.length;
+       int m=heights[0].length;
+       if(row<0 || row>=n||col<0||col>=m||arr[row][col]==1||height>heights[row][col]){
+        return;
+       }
+       arr[row][col]=1;
+       for(int i=0;i<4;i++){
+        int nrow=row+deltarow[i];
+        int ncol=col+deltacol[i];
+        dfs(nrow,ncol,heights[row][col],arr,heights);
+       }
+
     }
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         int n=heights.length;
         int m=heights[0].length;
-        int deltarow[]={-1,0,1,0};
-        int deltacol[]={0,1,0,-1};
         int pacific[][]=new int[n][m];
         int atlantic[][]=new int[n][m];
-        List<List<Integer>> ans=new ArrayList<>();
-        //checking first col and last col;
+        //checking first col and last col
         for(int i=0;i<n;i++){
-            dfs(heights,i,0,pacific,Integer.MIN_VALUE,deltarow,deltacol);
-            dfs(heights,i,m-1,atlantic,Integer.MIN_VALUE,deltarow,deltacol);
+            dfs(i,0,Integer.MIN_VALUE,pacific,heights);
+            dfs(i,m-1,Integer.MIN_VALUE,atlantic,heights);
         }
-        //checking first and last row
-        for(int j=0;j<m;j++){
-            dfs(heights,0,j,pacific,Integer.MIN_VALUE,deltarow,deltacol);
-            dfs(heights,n-1,j,atlantic,Integer.MIN_VALUE,deltarow,deltacol);
+
+        //checking first row and last row
+        for(int i=0;i<m;i++){
+            dfs(0,i,Integer.MIN_VALUE,pacific,heights);
+            dfs(n-1,i,Integer.MIN_VALUE,atlantic,heights);
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -36,7 +38,7 @@ class Solution {
                     List<Integer> temp=new ArrayList<>();
                     temp.add(i);
                     temp.add(j);
-                    ans.add(temp);
+                    ans.add(new ArrayList<>(temp));
                 }
             }
         }
