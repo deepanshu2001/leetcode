da6975daf443f6c9c530a8f834845bdf91f7039a
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-27 14:02:53 -0800

    Time: 41 ms (82.77%), Space: 55.1 MB (89%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.java
@@ -1,36 +1,15 @@
 class Solution {
-
     public List<String> removeSubfolders(String[] folder) {
-        // Create a set to store all folder paths for fast lookup
-        Set<String> folderSet = new HashSet<>(Arrays.asList(folder));
-        List<String> result = new ArrayList<>();
-
-        // Iterate through each folder to check if it's a sub-folder
-        for (String f : folder) {
-            boolean isSubFolder = false;
-            String prefix = f;
-
-            // Check all prefixes of the current folder path
-            while (!prefix.isEmpty()) {
-                int pos = prefix.lastIndexOf('/');
-                if (pos == -1) break;
-
-                // Reduce the prefix to its parent folder
-                prefix = prefix.substring(0, pos);
-
-                // If the parent folder exists in the set, mark as sub-folder
-                if (folderSet.contains(prefix)) {
-                    isSubFolder = true;
-                    break;
-                }
-            }
-
-            // If not a sub-folder, add it to the result
-            if (!isSubFolder) {
-                result.add(f);
+        List<String> ans=new ArrayList<>();
+        Arrays.sort(folder);
+        ans.add(folder[0]);
+        for(int i=1;i<folder.length;i++){
+            String s=folder[i];
+            String pre=ans.get(ans.size()-1)+"/";
+            if(!s.startsWith(pre)){
+                ans.add(s);
             }
         }
-
-        return result;
+        return ans;
     }
 }
