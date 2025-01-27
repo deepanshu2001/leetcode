00f891ac716d7b2969b0d9bfc7b6e4d5b269936a
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-27 12:52:42 -0800

    Time: 882 ms (5.02%), Space: 45.8 MB (7.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,23 +1,26 @@
-import java.util.HashMap;
-import java.util.Map;
-
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        Map<Character, Integer> map1 = new HashMap<>();
-        for (int i = 0; i < s1.length(); i++) {
-            char ch = s1.charAt(i);
-            map1.put(ch, map1.getOrDefault(ch, 0) + 1);
+        if(s1.equals(s2)){
+            return true;
         }
+        Map<Character,Integer> map=new HashMap<>();
+        for(int i=0;i<s1.length();i++){
+            char ch=s1.charAt(i);
+            map.put(ch,map.getOrDefault(ch,0)+1);
+        }
+        int len=s1.length();
+        for(int j=0;j<=s2.length()-len;j++){
+            String s=s2.substring(j,j+len);
+            Map<Character,Integer> new_map=new HashMap<>();
+            for(int k=0;k<s.length();k++){
+                char new_cha=s.charAt(k);
+                new_map.put(new_cha,new_map.getOrDefault(new_cha,0)+1);
 
-        for (int i = 0; i <= s2.length() - s1.length(); i++) {
-            Map<Character, Integer> map2 = new HashMap<>();
-            for (int j = 0; j < s1.length(); j++) {
-                map2.put(s2.charAt(i + j), map2.getOrDefault(s2.charAt(i + j), 0) + 1);
             }
-            // Compare map1 and map2 outside the inner loop
-            if (map1.equals(map2)) {
+            if(map.equals(new_map)){
                 return true;
             }
+            
         }
         return false;
     }
