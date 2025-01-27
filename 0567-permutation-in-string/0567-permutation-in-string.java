22d5e551fa68d01bd97f2f8ad13bf35806c1eedb
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-27 13:00:46 -0800

    Time: 27 ms (32.27%), Space: 44.4 MB (30.15%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -3,24 +3,40 @@ class Solution {
         if(s1.equals(s2)){
             return true;
         }
-        Map<Character,Integer> map=new HashMap<>();
+        Map<Character,Integer> map1=new HashMap<>();
         for(int i=0;i<s1.length();i++){
             char ch=s1.charAt(i);
-            map.put(ch,map.getOrDefault(ch,0)+1);
+            map1.put(ch,map1.getOrDefault(ch,0)+1);
         }
-        int len=s1.length();
-        for(int j=0;j<=s2.length()-len;j++){
-            String s=s2.substring(j,j+len);
-            Map<Character,Integer> new_map=new HashMap<>();
-            for(int k=0;k<s.length();k++){
-                char new_cha=s.charAt(k);
-                new_map.put(new_cha,new_map.getOrDefault(new_cha,0)+1);
+        int k=s1.length();
+        Map<Character,Integer> map2=new HashMap<>();
+        int j=0;
+        int i=0;
+        while(j<s2.length()){
+            char ch=s2.charAt(j);
+            map2.put(ch,map2.getOrDefault(ch,0)+1);
+            if(j-i+1==k){
+                if(map1.equals(map2)){
+                    return true;
+                }
+            }
+            if(j-i+1>k){
+                while(j-i+1>k){
+                    char new_char=s2.charAt(i);
+                    map2.put(new_char,map2.getOrDefault(new_char,0)-1);
+                    if(map2.get(new_char)==0){
+                        map2.remove(new_char);
+                    }
+                    i++;
+                }
+                if(j-i+1==k){
+                    if(map1.equals(map2)){
+                        return true;
+                    }
+                }
 
             }
-            if(map.equals(new_map)){
-                return true;
-            }
-            
+            j++;
         }
         return false;
     }
