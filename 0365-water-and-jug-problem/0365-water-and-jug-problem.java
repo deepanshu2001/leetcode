0abd7c85177f5d66aff041429b75dc2e50eaf701
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-01-28 12:09:02 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0365-water-and-jug-problem/0365-water-and-jug-problem.java
+++ 0365-water-and-jug-problem/0365-water-and-jug-problem.java
@@ -2,81 +2,65 @@ class Solution {
     class Pair{
         int first;
         int second;
-        public Pair(int first,int second){
+        Pair(int first,int second){
             this.first=first;
             this.second=second;
         }
     }
     public boolean canMeasureWater(int x, int y, int target) {
-        Queue<Pair> queue=new LinkedList<>();
         if(x+y==target){
             return true;
         }
         if(x+y<target){
             return false;
         }
-        queue.add(new Pair(0,0));
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(0,0));
         Set<String> set=new HashSet<>();
-        while(!queue.isEmpty()){
-            Pair p=queue.remove();
-            int curr_x=p.first;
-            int curr_y=p.second;
-            if(curr_x+curr_y==target){
+        while(!q.isEmpty()){
+            Pair pair=q.remove();
+            int x1=pair.first;
+            int y1=pair.second;
+            if(x1+y1==target){
                 return true;
             }
-            set.add(curr_x+","+curr_y);
-            //case 1 x->y
-            int new_x=curr_x-Math.min(curr_x,y-curr_y);
-            int new_y=curr_y+Math.min(curr_x,y-curr_y);
-            if(!set.contains(new_x+","+new_y)){
-                queue.add(new Pair(new_x,new_y));
+            set.add(x1+"->"+y1);
+            //case1 empty x;
+            int new_x=0;
+            int new_y=y1;
+            if(!set.contains(new_x+"->"+new_y)){
+                q.add(new Pair(new_x,new_y));
             }
-            new_x=0;
+            //case 2 empty y;
+            new_x=x1;
             new_y=0;
-            //case 2 x<-y
-            new_x=curr_x+Math.min(curr_x,x-curr_x);
-            new_y=curr_y-Math.min(curr_y,x-curr_x);
-            if(!set.contains(new_x+","+new_y)){
-                queue.add(new Pair(new_x,new_y));
+            if(!set.contains(new_x+"->"+new_y)){
+                q.add(new Pair(new_x,new_y));
             }
-            new_x=0;
-            new_y=0;
-            //case 3 empty x;
-            new_x=0;
-            new_y=curr_y;
-            if(!set.contains(new_x+","+new_y)){
-                queue.add(new Pair(new_x,new_y));
+            //case 3 fill x completely
+            new_x=x;
+            new_y=y1;
+            if(!set.contains(new_x+"->"+new_y)){
+                q.add(new Pair(new_x,new_y));
             }
-           
-            new_x=0;
-            new_y=0;
-             //case 4 empty y;
-             new_x=curr_x;
-             new_y=0;
-             if(!set.contains(new_x+","+new_y)){
-                queue.add(new Pair(new_x,new_y));
-             }
-             new_x=0;
-             new_y=0;
-             //case 5 fill x complexty;
-             new_x=x;
-             new_y=curr_y;
-             if(!set.contains(new_x+","+new_y)){
-                queue.add(new Pair(new_x,new_y));
-             }
-
-             new_x=0;
-             new_y=0;
-             //case 6 fill y complexty;
-             new_x=curr_x;
-             new_y=y;
-             if(!set.contains(new_x+","+new_y)){
-                queue.add(new Pair(new_x,new_y));
-             }
-
-
-            
-            
+            //case 4 fill y completely
+            new_x=x1;
+            new_y=y;
+            if(!set.contains(new_x+"->"+new_y)){
+                q.add(new Pair(new_x,new_y));
+            }
+            //case 5 x->y x fills y;
+            new_x=x1-Math.min(x1,y-y1);
+            new_y=y1+Math.min(x1,y-y1);
+            if(!set.contains(new_x+"->"+new_y)){
+                q.add(new Pair(new_x,new_y));
+            }
+            //case 6 x<-y y fills x completely
+            new_x=x1+Math.min(y1,y-y1);
+            new_y=y1-Math.min(y1,x-x1);
+            if(!set.contains(new_x+"->"+new_y)){
+                q.add(new Pair(new_x,new_y));
+            }
         }
         return false;
     }
