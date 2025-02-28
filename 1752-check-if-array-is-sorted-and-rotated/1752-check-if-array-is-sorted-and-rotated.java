375b4ce642fea73fd987acc2a12dfdd354caa192
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-02-28 11:32:29 -0800

    Time: 0 ms (100%), Space: 41.4 MB (39.27%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,22 +1,16 @@
 class Solution {
     public boolean check(int[] nums) {
-        int rotationCount = 0;
-        int n = nums.length;
-
-        // Traverse the array to count the number of rotation points
-        for (int i = 0; i < n; i++) {
-            // Check if the current element is greater than the next element (modulus for circular check)
-            if (nums[i] > nums[(i + 1) % n]) {
-                rotationCount++;
+        int rotation_cnt=0;
+        int n=nums.length;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>nums[(i+1)%n]){
+                rotation_cnt++;
             }
 
-            // If there is more than one rotation point, it's not a rotated sorted array
-            if (rotationCount > 1) {
+            if(rotation_cnt>1){
                 return false;
             }
         }
-
-        // If zero or one rotation point is found, it's a rotated sorted array
         return true;
     }
 }
