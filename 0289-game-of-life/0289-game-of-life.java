302b3473f1d64177c59197b9d5e6616c389760df
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-03-03 13:35:54 -0800

    Time: 0 ms (100%), Space: 41.8 MB (55.02%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -2,17 +2,8 @@ class Solution {
     public void gameOfLife(int[][] board) {
         int n = board.length;
         int m = board[0].length;
-        int vis[][] = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                vis[i][j] = board[i][j];
-            }
-        }
-
         int deltarow[] = { -1, -1, -1, 0, 1, 1, 1, 0 };
         int deltacol[] ={ -1, 0, 1, 1, 1, 0, -1, -1 };
-
-        // Traverse board to apply game rules
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 int cnt = 0;
@@ -20,18 +11,28 @@ class Solution {
                 for (int k = 0; k < 8; k++) {
                     int nrow = i + deltarow[k];
                     int ncol = j + deltacol[k];
-                    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && vis[nrow][ncol] == 1) {
+                    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && Math.abs(board[nrow][ncol]) == 1) {
                         cnt++;
                     }
                 }
-                if (vis[i][j] == 0) { 
+                if (board[i][j] ==0) { 
                     if (cnt == 3) {
-                        board[i][j] = 1; 
+                        board[i][j] =2; 
                     }
                 } else { 
                     if (cnt < 2 || cnt > 3) {
-                        board[i][j] = 0;
+                        board[i][j] =-1;
                     }
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]>0){
+                    board[i][j]=1;
+                }
+                else{
+                    board[i][j]=0;
                 }
             }
         }
