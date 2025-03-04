735534ababdbccf058ef40a1d03a4e9d94c880a7
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-03-03 17:35:30 -0800

    Time: 3 ms (25.01%), Space: 45.7 MB (75.83%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -1,7 +1,3 @@
-
-
-import static java.lang.Math.log;
-
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -18,38 +14,34 @@ import static java.lang.Math.log;
  * }
  */
 class Solution {
+    List<List<Integer>> list=new ArrayList<>();
+    public void dfs(TreeNode node,int level){
+       if(list.size()==level){
+        list.add(new ArrayList<>());
+       }
+       list.get(level).add(node.val);
+       if(node.left!=null){
+        dfs(node.left,level+1);
+       }
+       if(node.right!=null){
+        dfs(node.right,level+1);
+       }
+    }
     public List<Double> averageOfLevels(TreeNode root) {
-       List<Double> ans=new ArrayList<>();
-       if(root==null){
-          return ans; 
-       }
-       List<List<Integer>> list=new ArrayList<>();
-       Queue<TreeNode> q=new LinkedList<>();
-       q.add(root);
-       while(!q.isEmpty()){
-         int size=q.size();
-         List<Integer> temp=new ArrayList<>();
-         for(int i=0;i<size;i++){
-            TreeNode node=q.remove();
-            temp.add(node.val);
-            if(node.left!=null){
-                q.add(node.left);
-            }
-            if(node.right!=null){
-                q.add(node.right);
-            }
-         }
-         list.add(new ArrayList<>(temp));
-       }
-       for(List<Integer> l:list){
-        int size=l.size();
-        long sum=0;
-        for(int num:l){
-            sum+=num;
+        List<Double> ans=new ArrayList<>();
+        if(root==null){
+            return ans;
         }
-        ans.add(sum*1.0/size);
-       }
-       return ans;
-      
+        List<Integer> temp=new ArrayList<>();
+        dfs(root,0);
+        for(List<Integer> l:list){
+            int size=l.size();
+            long sum=0;
+            for(int num:l){
+              sum+=num;
+            }
+            ans.add(sum*1.0/size);
+        }
+        return ans;
     }
 }
