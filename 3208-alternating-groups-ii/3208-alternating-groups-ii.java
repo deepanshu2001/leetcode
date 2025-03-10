17684cafc6164d11720badfb835f4f8aa73e6276
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-03-10 12:05:26 -0700

    Time: 1 ms (100%), Space: 63.1 MB (49.86%) - LeetHub

--- 3208-alternating-groups-ii/3208-alternating-groups-ii.java
+++ 3208-alternating-groups-ii/3208-alternating-groups-ii.java
@@ -4,12 +4,9 @@ class Solution {
         if (k == 0 || colors.length == 0) {
             return 0;
         }
-
         int cnt = 0;
-        int alternatingCount = 1; // length of current alternating sequence
+        int alternatingCount = 1; 
         int lastColor = colors[0];
-
-        // First, linear traversal
         for (int j = 1; j < colors.length; j++) {
             if (colors[j] != lastColor) {
                 alternatingCount++;
@@ -17,13 +14,10 @@ class Solution {
                     cnt++;
                 }
             } else {
-                // Pattern breaks
                 alternatingCount = 1;
             }
             lastColor = colors[j];
         }
-
-        // Now, handle circular wrap-around for first k-1 elements
         for (int j = 0; j < k - 1; j++) {
             if (colors[j] != lastColor) {
                 alternatingCount++;
@@ -31,7 +25,7 @@ class Solution {
                     cnt++;
                 }
             } else {
-                break; // Pattern breaks, can't extend further
+                break;
             }
             lastColor = colors[j];
         }
