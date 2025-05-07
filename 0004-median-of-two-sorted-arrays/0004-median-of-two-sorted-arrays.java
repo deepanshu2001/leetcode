bb0c3811ddba7d04ce66e8ab10b5ad0b07cea557
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-05-07 10:47:39 -0700

    Time: 1 ms (100%), Space: 46.2 MB (44.35%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,60 +1,38 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int ind1=0;
-        int ind2=0;
-        int elem1=0;
-        int elem2=0;
-        int i=0;
-        int j=0;
-        int cnt=0;
-        ind2=(nums1.length+nums2.length)/2;
-        ind1=ind2-1;
-        while(i<nums1.length && j<nums2.length){
-            if(nums1[i]<nums2[j]){
-                if(cnt==ind1){
-                    elem1=nums1[i];
+        if(nums1.length>nums2.length){
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int m=nums1.length;
+        int n=nums2.length;
+        int l=0;
+        int r=m;
+        while(l<=r){
+            int px=l+(r-l)/2;
+            int py=(m+n+1)/2-px;
+            //left side
+            int x1=px==0?Integer.MIN_VALUE:nums1[px-1];
+            int x2=py==0?Integer.MIN_VALUE:nums2[py-1];
+            //right half
+            int x3=px==m?Integer.MAX_VALUE:nums1[px];
+            int x4=py==n?Integer.MAX_VALUE:nums2[py];
+            
+
+            if(x1<=x4 && x2<=x3){
+                if((m+n)%2==1){
+                    return Math.max(x1, x2);
                 }
-                if(cnt==ind2){
-                    elem2=nums1[i];
-                }
-                cnt++;
-                i++;
+                return (Math.max(x1,x2)+Math.min(x3,x4))/2.0;
+            }
+
+            if(x1>x4){
+                r=px-1;
             }
             else{
-                if(cnt==ind1){
-                    elem1=nums2[j];
-                }
-                if(cnt==ind2){
-                    elem2=nums2[j];
-                }
-                cnt++;
-                j++;
+                l=px+1;
             }
-        }
-        while(i<nums1.length){
-            if(cnt==ind1){
-                elem1=nums1[i];
-            }
-            if(cnt==ind2){
-                elem2=nums1[i];
-            }
-            cnt++;
-            i++;
-        }
-        while(j<nums2.length){
-            if(cnt==ind1){
-                elem1=nums2[j];
-            }
-            if(cnt==ind2){
-                elem2=nums2[j];
-            }
-            cnt++;
-            j++;
-        }
-        if((nums1.length+nums2.length)%2==1){
-            return (double)elem2;
-        }
-        return (double)(elem1+elem2)/2;
 
+        }
+        return -1;
     }
 }
