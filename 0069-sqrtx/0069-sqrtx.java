40a4515f6edc6817cb062f3f206d65f9c8e63dac
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-05-20 09:15:14 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,24 +1,14 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x<=1){
-            return x;
+        if(x==0){
+            return 0;
         }
-        int s=1;
-        int e=x;
-        
-        while(s<=e){
-            int mid=s+(e-s)/2;
-            if(mid>x/mid){
-                e=mid-1;
-            }
-            else if(mid<x/mid){
-                
-                s=mid+1;
-            }
-            else if(mid==x/mid){
-                return mid;
+        int ans=1;
+        for(int i=1;i<x;i++){
+            if(i*i<=x){
+                ans=i;
             }
         }
-        return e;
+        return ans;
     }
 }
