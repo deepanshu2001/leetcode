2dbbebb52aaf830032cbfd8829b8cb1133dd60a3
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-06-20 10:20:11 -0700

    Time: 1 ms (72.17%), Space: 45.5 MB (80.92%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,22 +2,34 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n=matrix[0].length;
-        int row[]=new int[m];
-        int col[]=new int[n];
+        boolean is_col=false;
         for(int i=0;i<m;i++){
-            for(int j=0;j<matrix[0].length;j++){
-                //mark cells as visited
+            if(matrix[i][0]==0){
+                is_col=true;
+            }
+            for(int j=1;j<n;j++){
                 if(matrix[i][j]==0){
-                    row[i]=1;
-                    col[j]=1;
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(row[i]==1||col[j]==1){
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(matrix[i][0]==0||matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
+            }
+        }
+        //first row
+        if(matrix[0][0]==0){
+           for(int i=0;i<n;i++){
+            matrix[0][i]=0;
+           }
+        }
+        if(is_col){
+            for(int i=0;i<m;i++){
+                matrix[i][0]=0;
             }
         }
     }
