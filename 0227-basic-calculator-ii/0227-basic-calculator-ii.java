6e7b1ee545937ee6bbbb8b1105dcf5ee5868e11b
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-07-22 12:21:32 -0700

    Time: 19 ms (62.84%), Space: 46.6 MB (35.16%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> st=new Stack<>();
-        int i=0;
-        char op='+';
-        while(i<s.length()){
-          char ch=s.charAt(i);
-          if(Character.isDigit(ch)){
+       Stack<Integer> st=new Stack<>();
+       int i=0;
+       char op='+';
+       while(i<s.length()){
+         char ch=s.charAt(i);
+         if(Character.isDigit(ch)){
             int num=0;
             while(i<s.length() && Character.isDigit(s.charAt(i))){
                 num=num*10+s.charAt(i)-'0';
@@ -23,26 +23,27 @@ class Solution {
             else if(op=='/'){
                 st.push(st.pop()/num);
             }
-          }else{
+         }
+         else{
             if(ch=='+'){
                 op='+';
             }
             else if(ch=='-'){
                 op='-';
             }
-            else if(ch=='*'){
-                op='*';
-            }
             else if(ch=='/'){
                 op='/';
             }
+            else if(ch=='*'){
+                op='*';
+            }
             i++;
-          }
-        }
-        int ans=0;
-        while(!st.isEmpty()){
-            ans+=st.pop();
-        }
-        return ans;
-    }
+         }
+       }  
+       int ans=0;
+       while(!st.isEmpty()){
+        ans+=st.pop();
+       }
+       return ans;
+    } 
 }
