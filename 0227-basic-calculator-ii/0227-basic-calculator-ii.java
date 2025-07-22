d4a9572ace2a4c9a38e27360de34cebf886e2f5b
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-07-22 12:21:05 -0700

    Time: 17 ms (71.47%), Space: 46.8 MB (27.57%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.java
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.java
@@ -1,31 +1,43 @@
 class Solution {
     public int calculate(String s) {
         Stack<Integer> st=new Stack<>();
-        char op='+';
-        int num=0;
         int i=0;
+        char op='+';
         while(i<s.length()){
-            char ch=s.charAt(i);
-            if(Character.isDigit(ch)){
-                num=num*10+ch-'0';
+          char ch=s.charAt(i);
+          if(Character.isDigit(ch)){
+            int num=0;
+            while(i<s.length() && Character.isDigit(s.charAt(i))){
+                num=num*10+s.charAt(i)-'0';
+                i++;
             }
-            if ((!Character.isDigit(ch) && ch != ' ') || i == s.length() - 1) {
-                if(op=='+'){
-                    st.push(num);
-                }
-                if(op=='-'){
-                    st.push(-num);
-                }
-                if(op=='*'){
-                    st.push(st.pop()*num);
-                }
-                if(op=='/'){
-                    st.push(st.pop()/num);
-                }
-                op=ch;
-                num=0;
+            if(op=='+'){
+                st.push(num);
+            }
+            else if(op=='-'){
+                st.push(-num);
+            }
+            else if(op=='*'){
+                st.push(st.pop()*num);
+            }
+            else if(op=='/'){
+                st.push(st.pop()/num);
+            }
+          }else{
+            if(ch=='+'){
+                op='+';
+            }
+            else if(ch=='-'){
+                op='-';
+            }
+            else if(ch=='*'){
+                op='*';
+            }
+            else if(ch=='/'){
+                op='/';
             }
             i++;
+          }
         }
         int ans=0;
         while(!st.isEmpty()){
