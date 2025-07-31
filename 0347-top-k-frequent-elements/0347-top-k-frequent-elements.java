c5c5550452e3118b57764379f02ec4e89d57a81a
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-07-31 09:59:18 -0700

    Time: 12 ms (82.65%), Space: 48.3 MB (79.59%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,31 +1,84 @@
 class Solution {
+    int[] unique;
+    Map<Integer, Integer> count;
+
+    public void swap(int a, int b) {
+        int tmp = unique[a];
+        unique[a] = unique[b];
+        unique[b] = tmp;
+    }
+
+    public int partition(int left, int right, int pivot_index) {
+        int pivot_frequency = count.get(unique[pivot_index]);
+        // 1. Move pivot to end
+        swap(pivot_index, right);
+        int store_index = left;
+
+        // 2. Move all less frequent elements to the left
+        for (int i = left; i <= right; i++) {
+            if (count.get(unique[i]) < pivot_frequency) {
+                swap(store_index, i);
+                store_index++;
+            }
+        }
+
+        // 3. Move the pivot to its final place
+        swap(store_index, right);
+
+        return store_index;
+    }
+    
+    public void quickselect(int left, int right, int k_smallest) {
+        /*
+        Sort a list within left..right till kth less frequent element
+        takes its place. 
+        */
+
+        // base case: the list contains only one element
+        if (left == right) return;
+        
+        //Select a random pivot_index
+        Random random_num = new Random();
+        int pivot_index = left + random_num.nextInt(right - left); 
+
+        // Find the pivot position in a sorted list
+        pivot_index = partition(left, right, pivot_index);
+
+        // If the pivot is in its final sorted position
+        if (k_smallest == pivot_index) {
+            return;    
+        } else if (k_smallest < pivot_index) {
+            // go left
+            quickselect(left, pivot_index - 1, k_smallest);     
+        } else {
+            // go right 
+            quickselect(pivot_index + 1, right, k_smallest);  
+        }
+    }
+    
     public int[] topKFrequent(int[] nums, int k) {
-        List<Integer>[] bucket=new List[nums.length+1];
-        Map<Integer,Integer> map=new HashMap<>();
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+        // Build hash map: character and how often it appears
+        count = new HashMap();
+        for (int num: nums) {
+            count.put(num, count.getOrDefault(num, 0) + 1);
         }
-        for(Map.Entry<Integer,Integer> m:map.entrySet()){
-            int elem=m.getKey();
-            int freq=m.getValue();
-            if(bucket[freq]==null){
-                bucket[freq]=new ArrayList<>();
-            }
-            bucket[freq].add(elem);
+        
+        // Array of unique elements
+        int n = count.size();
+        unique = new int[n]; 
+        int i = 0;
+        for (int num: count.keySet()) {
+            unique[i] = num;
+            i++;
         }
-        int ans[]=new int[k];
-        int ind=0;
-        for(int i=nums.length;i>=0;i--){
-            if(bucket[i]!=null){
-                for(int j=0;j<bucket[i].size() && ind<k;j++){
-                    ans[ind]=bucket[i].get(j);
-                    ind++;
-                    if(ind==k){
-                        break;
-                    }
-                }
-            }
-        }
-        return ans;
+        
+        // kth top frequent element is (n - k)th less frequent.
+        // Do a partial sort: from less frequent to the most frequent, till
+        // (n - k)th less frequent element takes its place (n - k) in a sorted array. 
+        // All elements on the left are less frequent.
+        // All the elements on the right are more frequent. 
+        quickselect(0, n - 1, n - k);
+        // Return top k frequent elements
+        return Arrays.copyOfRange(unique, n - k, n);
     }
 }
