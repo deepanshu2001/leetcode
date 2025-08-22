a3cbfe35d878eb5dd7f141fbf4727073dfce8a85
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-08-22 09:48:27 -0700

    Time: 1 ms (38.6%), Space: 43.1 MB (78.67%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,15 +1,20 @@
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        //space optimiation'
-        int n=cost.length;
-        int prev1=cost[0];
-        int prev2=cost[1];
-        for(int i=2;i<n;i++){
-           int curr=cost[i]+Math.min(prev1,prev2);
-           prev1=prev2;
-           prev2=curr;
+    public int f(int ind,int cost[],int dp[]){
+        if(ind>=cost.length){
+            return 0;
         }
-        return Math.min(prev1,prev2);
+
+        if(dp[ind]!=-1){
+            return dp[ind];
+        }
+        int one_step=cost[ind]+f(ind+1,cost,dp);
+        int two_step=cost[ind]+f(ind+2,cost,dp);
+        return dp[ind]= Math.min(one_step,two_step);
+    }
+    public int minCostClimbingStairs(int[] cost) {
+        int []dp=new int[cost.length+1];
+        Arrays.fill(dp,-1);
+        return Math.min(f(0,cost,dp),f(1,cost,dp));
 
     }
 }
