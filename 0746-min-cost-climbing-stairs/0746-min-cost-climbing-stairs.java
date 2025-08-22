be420a068f8eee114a4b5e044589bb68fba569a8
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-08-22 09:51:03 -0700

    Time: 0 ms (100%), Space: 43.5 MB (27.17%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -13,8 +13,15 @@ class Solution {
     }
     public int minCostClimbingStairs(int[] cost) {
         int []dp=new int[cost.length+1];
-        Arrays.fill(dp,-1);
-        return Math.min(f(0,cost,dp),f(1,cost,dp));
+        int n=cost.length;
+        dp[n-1]=cost[n-1];
+        for(int i=n-2;i>=0;i--){
+           int one_step=cost[i]+dp[i+1];
+           int two_step=cost[i]+dp[i+2];
+           dp[i]=Math.min(one_step,two_step);
+        }
+        return Math.min(dp[0],dp[1]);
+       
 
     }
 }
