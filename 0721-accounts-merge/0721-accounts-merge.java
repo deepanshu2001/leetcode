da69cb3abfbea12170c70dd55a37bae19ab3eee5
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-09-08 11:44:34 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,90 +1,88 @@
-public class DisjointSet {
-    List<Integer> parent=new ArrayList<>();
-    List<Integer> rank=new ArrayList<>();
-    List<Integer> size=new ArrayList<>();
-    public DisjointSet(int n){
-        for(int i=0;i<=n;i++){
-            parent.add(i);
+class DisjointSet {
+    List<Integer> rank = new ArrayList<>();
+    List<Integer> parent = new ArrayList<>();
+    List<Integer> size = new ArrayList<>();
+    public DisjointSet(int n) {
+        for(int i = 0;i<=n;i++) {
             rank.add(0);
+            parent.add(i);
             size.add(1);
         }
     }
-    public int findUPar(int node){
-        if(node==parent.get(node)){
+
+    public int findUPar(int node) {
+        if(node == parent.get(node)) {
             return node;
         }
-        int Upar=findUPar(parent.get(node));
-        parent.set(node,Upar);
+        int ulp = findUPar(parent.get(node));
+        parent.set(node, ulp);
         return parent.get(node);
+    }
 
-    }
-    public void unionByRank(int u,int v){
-        int up=findUPar(u);
-        int vp=findUPar(v);
-        if(up==vp){
-            return;
+    public void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(rank.get(ulp_u) < rank.get(ulp_v)) {
+            parent.set(ulp_u, ulp_v);
         }
-        if(rank.get(up)<rank.get(vp)){
-            parent.set(up,vp);
+        else if(rank.get(ulp_v) < rank.get(ulp_u)) {
+            parent.set(ulp_v, ulp_u);
         }
-        else if(rank.get(vp)<rank.get(up)){
-            parent.set(vp,up);
-        }
-        else{
-            parent.set(vp,up);
-            int rankU=rank.get(up);
-            rank.set(up,rankU+1);
+        else {
+            parent.set(ulp_v, ulp_u);
+            int rankU = rank.get(ulp_u);
+            rank.set(ulp_u, rankU + 1);
         }
     }
-    public void unionBySize(int u,int v){
-        int up=findUPar(u);
-        int vp=findUPar(v);
-        if(up==vp){
-            return;
-        }
-        if(size.get(up)<size.get(vp)){
-            parent.set(up,vp);
-            size.set(vp,size.get(up)+size.get(vp));
+    public void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if(ulp_u == ulp_v) return;
+        if(size.get(ulp_u)<size.get(ulp_v)){
+            parent.set(ulp_u,ulp_v);
+            size.set(ulp_v,size.get(ulp_u)+size.get(ulp_v));
         }
         else{
-            parent.set(vp,up);
-            size.set(up,size.get(up)+size.get(vp));
+            parent.set(ulp_v,ulp_u);
+            size.set(ulp_u,size.get(ulp_u)+size.get(ulp_v));
         }
     }
 }
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
         int n=accounts.size();
-        Map<String,Integer> map=new HashMap<>();
         DisjointSet ds=new DisjointSet(n);
-        for(int i=0;i<n;i++){
+        List<List<String>> ans=new ArrayList<>();
+        Map<String,Integer> map=new HashMap<>();
+        for(int i=0;i<accounts.size();i++){
             for(int j=1;j<accounts.get(i).size();j++){
                 String mail=accounts.get(i).get(j);
-                if(map.containsKey(mail)==false){
+                if(!map.containsKey(mail)){
                     map.put(mail,i);
-                }else{
+                }
+                else{
                     ds.unionBySize(i,map.get(mail));
                 }
             }
         }
-        ArrayList<String>[] mergeMail=new ArrayList[n];
+        ArrayList<String>[] mails=new ArrayList[n];
         for(int i=0;i<n;i++){
-            mergeMail[i]=new ArrayList<>();
-        }
-        for(Map.Entry<String,Integer> it:map.entrySet()){
-            String mail=it.getKey();
+            mails.add(new ArrayList<>());
+        } 
+        for(Map.Entry<String,Integer> m:map.entrySet()){
+            String mail=m.getKey();
             int node=ds.findUPar(it.getValue());
-            mergeMail[node].add(mail);
+            mails[node].add(mail);
         }
-        List<List<String>> ans=new ArrayList<>();
         for(int i=0;i<n;i++){
-            if(mergeMail[i].size()==0){
+            if(mails[i].size()==0){
                 continue;
             }
-            Collections.sort(mergeMail[i]);
+            Collections.sort(mails[i]);
             ArrayList<String> temp=new ArrayList<>();
             temp.add(accounts.get(i).get(0));
-            for(String it:mergeMail[i]){
+            for(String it:mails[i]){
                 temp.add(it);
             }
             ans.add(temp);
