2fed23f4f3c74fbe25f73c09409cd17bf6814aed
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-10-03 11:16:00 -0700

    Time: 106 ms (5.07%), Space: 56.1 MB (6.51%) - LeetHub

--- 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.java
@@ -1,55 +1,74 @@
 class Solution {
-    public String longestWord(String[] words) {
-        Trie trie = new Trie();
-        int index = 0;
-        for (String word: words) {
-            trie.insert(word, ++index); //indexed by 1
+    class Node{
+        Node links[]=new Node[26];
+        boolean flag=false;
+        Node(){
+
         }
-        trie.words = words;
-        return trie.dfs();
-    }
-}
-class Node {
-    char c;
-    HashMap<Character, Node> children = new HashMap();
-    int end;
-    public Node(char c){
-        this.c = c;
-    }
-}
-
-class Trie {
-    Node root;
-    String[] words;
-    public Trie() {
-        root = new Node('0');
-    }
-
-    public void insert(String word, int index) {
-        Node cur = root;
-        for (char c: word.toCharArray()) {
-            cur.children.putIfAbsent(c, new Node(c));
-            cur = cur.children.get(c);
+        public boolean containsKey(char ch){
+            return links[ch-'a']!=null;
         }
-        cur.end = index;
+        public void put(char ch,Node node){
+            links[ch-'a']=node;
+        }
+        public Node get(char ch){
+            return links[ch-'a'];
+        }
+        public void setEnd(){
+            flag=true;
+        }
+        public boolean isEnd(){
+            return flag==true;
+        }
     }
-
-    public String dfs() {
-        String ans = "";
-        Stack<Node> stack = new Stack();
-        stack.push(root);
-        while (!stack.empty()) {
-            Node node = stack.pop();
-            if (node.end > 0 || node == root) {
-                if (node != root) {
-                    String word = words[node.end - 1];
-                    if (word.length() > ans.length() ||
-                            word.length() == ans.length() && word.compareTo(ans) < 0) {
-                        ans = word;
-                    }
+    class Trie{
+        Node root;
+        Trie(){
+            root=new Node();
+        }
+        public void insert(String word){
+            Node node=root;
+            for(int i=word.length()-1;i>=0;i--){
+                char ch=word.charAt(i);
+                if(!node.containsKey(ch)){
+                    node.put(ch,new Node());
                 }
-                for (Node nei: node.children.values()) {
-                    stack.push(nei);
+                node=node.get(ch);
+            }
+            node.setEnd();
+        }
+        public boolean containsWord(String word){
+            Node node=root;
+            for(int i=word.length()-1;i>=0;i--){
+                char ch=word.charAt(i);
+                if(!node.containsKey(ch)){
+                    return false;
+                }
+                node=node.get(ch);
+            }
+            return node.isEnd();
+        }
+        
+    }
+    public String longestWord(String[] words) {
+        Trie trie=new Trie();
+        for(String word:words){
+            trie.insert(word);
+        }
+        String ans="";
+        for(String word:words){
+            String s="";
+            boolean flag=true;
+            for(int i=0;i<word.length();i++){
+                s+=word.charAt(i);
+                if(!trie.containsWord(s)){
+                    flag=false;
+                    break;
+                }
+            }
+            if(flag){
+                if(ans.length()<word.length()||(ans.length()==word.length() && word.compareTo(ans)<0)){
+                    ans=word;
                 }
             }
         }
