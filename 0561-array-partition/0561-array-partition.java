91e720375627b9adaa164131224a3938735dd5d6
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-10-09 11:21:45 -0700

    Time: 4 ms (99.86%), Space: 46.5 MB (89.46%) - LeetHub

--- 0561-array-partition/0561-array-partition.java
+++ 0561-array-partition/0561-array-partition.java
@@ -1,14 +1,31 @@
 class Solution {
     public int arrayPairSum(int[] nums) {
-        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-        for(int num:nums){
-            pq.add(num);
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            max = Math.max(max, num);
+            min = Math.min(min, num);
         }
-        int ans=0;
-        while(!pq.isEmpty()){
-            pq.remove();
-            ans+=pq.remove();
+
+        int[] count = new int[max - min + 1];
+        for (int num : nums) {
+            count[num - min]++;
         }
+
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                nums[index++] = i + min; // convert back to original number
+                count[i]--;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            ans += nums[i];
+        }
+
         return ans;
     }
 }
