f488f6e14c30a86738e006ec5db63bba108f1bb6
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-10-16 12:31:26 -0700

    Time: 1 ms (58.49%), Space: 41.9 MB (32.16%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,29 +1,74 @@
 class Solution {
+    class Node{
+        Node []links=new Node[26];
+        int size=0;
+        boolean flag=false;
+        Node(){
+
+        }
+        public boolean containsKey(char ch){
+            return links[ch-'a']!=null;
+        }
+        public void put(char ch,Node node){
+            links[ch-'a']=node;
+            size++;
+        }
+        public Node get(char ch){
+            return links[ch-'a'];
+        }
+        public boolean isEnd(){
+            return flag==true;
+        }
+        public void setEnd(){
+            flag=true;
+        }
+        public int getSize(){
+            return size;
+        }
+    }
+    class Trie{
+        Node root;
+        Trie(){
+            root=new Node();
+        }
+        public void insertWord(String word){
+            Node node=root;
+            for(int i=0;i<word.length();i++){
+                char ch=word.charAt(i);
+                if(!node.containsKey(ch)){
+                    node.put(ch,new Node());
+                }
+                node=node.get(ch);
+            }
+            node.setEnd();
+        }
+        public String containsPrefix(String word){
+            StringBuilder prefix=new StringBuilder();
+            Node node=root;
+            for(int i=0;i<word.length();i++){
+                char ch=word.charAt(i);
+                if(node.containsKey(ch) && node.getSize()==1 && !node.isEnd()){
+                    prefix.append(ch);
+                    node=node.get(ch);
+                }
+                else{
+                    return prefix.toString();
+                }
+            }
+            return prefix.toString();
+        }
+    }
     public String longestCommonPrefix(String[] strs) {
+        if(strs.length==1){
+            return strs[0];
+        }
         if(strs.length==0){
             return "";
         }
-        return lcs(strs,0,strs.length-1);
+        Trie trie=new Trie();
+        for(int i=1;i<strs.length;i++){
+            trie.insertWord(strs[i]);
+        }
+        return trie.containsPrefix(strs[0]);
     }
-    public String lcs(String []strs,int l,int r){
-        if(l==r){
-            return strs[l];
-        }
-        else{
-            int mid=(l+r)/2;
-            String left=lcs(strs,l,mid);
-            String right=lcs(strs,mid+1,r);
-            return commonPrefix(left,right);
-        }
-       
-    }
-     public String commonPrefix(String left,String right){
-            int min=Math.min(left.length(),right.length());
-            for(int i=0;i<min;i++){
-                if(left.charAt(i)!=right.charAt(i)){
-                    return left.substring(0,i);
-                }
-            }
-            return left.substring(0,min);
-        }
 }
