53c4a0a88f908046457f6587a15c16ac6e717cdc
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-10-24 12:39:51 -0700

    Time: 354 ms (33.56%), Space: 114.5 MB (18.69%) - LeetHub

--- 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.java
+++ 3342-find-minimum-time-to-reach-last-room-ii/3342-find-minimum-time-to-reach-last-room-ii.java
@@ -1,55 +1,51 @@
 class Solution {
-
-    private static final int INF = 0x3f3f3f3f;
-
-    class State implements Comparable<State> {
-
-        int x, y, dis;
-
-        State(int x, int y, int dis) {
-            this.x = x;
-            this.y = y;
-            this.dis = dis;
-        }
-
-        @Override
-        public int compareTo(State other) {
-            return Integer.compare(this.dis, other.dis);
+    class Pair{
+        int first;
+        int second;
+        int third;
+        Pair(int first,int second,int third){
+            this.first=first;
+            this.second=second;
+            this.third=third;
         }
     }
-
     public int minTimeToReach(int[][] moveTime) {
-        int n = moveTime.length, m = moveTime[0].length;
-        int[][] d = new int[n][m];
-        boolean[][] v = new boolean[n][m];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(d[i], INF);
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.first-b.first);
+        int n=moveTime.length;
+        int m=moveTime[0].length;
+        int vis[][]=new int[n][m];
+        int dist[][]=new int[n][m];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                dist[i][j]=Integer.MAX_VALUE;
+            }
         }
-
-        int[][] dirs = { { 1, 0 }, { -1, 0 }, { 0, 1 }, { 0, -1 } };
-        d[0][0] = 0;
-        PriorityQueue<State> q = new PriorityQueue<>();
-        q.offer(new State(0, 0, 0));
-        while (!q.isEmpty()) {
-            State s = q.poll();
-            if (v[s.x][s.y]) continue;
-            if (s.x == n - 1 && s.y == m - 1) break;
-            v[s.x][s.y] = true;
-
-            for (int[] dir : dirs) {
-                int nx = s.x + dir[0];
-                int ny = s.y + dir[1];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                int dist =
-                    Math.max(d[s.x][s.y], moveTime[nx][ny]) +
-                    ((s.x + s.y) % 2) +
-                    1;
-                if (d[nx][ny] > dist) {
-                    d[nx][ny] = dist;
-                    q.offer(new State(nx, ny, dist));
+        dist[0][0]=0;
+        int deltarow[]={-1,0,1,0};
+        int deltacol[]={0,1,0,-1};
+       
+        pq.add(new Pair(0,0,0));
+        while(!pq.isEmpty()){
+            Pair pair=pq.remove();
+            int time=pair.first;
+            int row=pair.second;
+            int col=pair.third;
+            if(vis[row][col]==1){
+                continue;
+            }
+            vis[row][col]=1;
+            for(int i=0;i<4;i++){
+                int nrow=row+deltarow[i];
+                int ncol=col+deltacol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                    int total_t=Math.max(time,moveTime[nrow][ncol])+((row+col)%2)+1;
+                    if(total_t<dist[nrow][ncol]){
+                        dist[nrow][ncol]=total_t;
+                        pq.add(new Pair(total_t,nrow,ncol));
+                    }
                 }
             }
         }
-        return d[n - 1][m - 1];
+        return dist[n-1][m-1];
     }
 }
