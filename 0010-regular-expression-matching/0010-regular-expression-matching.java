4ab0a41914b814dd26ebb57eb07b5dca68486a5d
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2025-12-29 11:19:28 -0800

    Time: 1095 ms (5.08%), Space: 47.1 MB (6.05%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean solve(String s,String p){
+        //base case
         if(p.length()==0){
             return s.length()==0;
         }
         boolean flag=false;
-        if(s.length()>0 &&(s.charAt(0)==p.charAt(0)||p.charAt(0)=='.')){
+        if(s.length()>0 && (s.charAt(0)==p.charAt(0)||p.charAt(0)=='.')){
             flag=true;
         }
         if(p.length()>1 && p.charAt(1)=='*'){
-            boolean nottake=solve(s,p.substring(2));
-            boolean take=flag&&solve(s.substring(1),p);
-            return take||nottake;
+            boolean notake=solve(s,p.substring(2));
+            boolean take=flag && solve(s.substring(1),p);
+            return take||notake;
         }
         else{
             return flag && solve(s.substring(1),p.substring(1));
