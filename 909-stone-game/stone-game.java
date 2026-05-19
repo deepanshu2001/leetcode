a6cf827b215aacc2d62849d75ba7be049acb51b5
deepanshu sharma <44342782+deepanshu2001@users.noreply.github.com>
2026-05-18 20:40:09 -0400

    Time: 9 ms (11.31%) | Memory: 46.6 MB (15.67%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,29 @@
+import java.util.*;
+
 class Solution {
+    public int f(int i, int j, int[] piles, int[][] dp) {
+        if (i == j) {
+            return piles[i];
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        int pickLeft = piles[i] - f(i + 1, j, piles, dp);
+        int pickRight = piles[j] - f(i, j - 1, piles, dp);
+
+        return dp[i][j] = Math.max(pickLeft, pickRight);
+    }
+
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length;
+        int[][] dp = new int[n][n];
+
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+
+        return f(0, n - 1, piles, dp) > 0;
     }
 }
